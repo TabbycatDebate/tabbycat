d3a6af5b1edb5b2f4014ca978e7b8cc910038580
Qi-Shan Lim <qishan@bios.net.nz>
2009-12-26 18:17:25 +1300

    fix random result generator - no draws

--- src/debate/utils.py
+++ src/debate/utils.py
@@ -2,29 +2,46 @@ def pair_list(ls):
     half = len(ls)/2
     return zip(ls[:half], ls[half:])
 
+def gen_results():
+    import random
+
+    r = {'aff': (0,), 'neg': (0,)}
+
+    def do():
+        s = [random.randint(60, 80) for i in range(3)]
+        s.append(random.randint(30,40))
+        return s
+
+    while sum(r['aff']) == sum(r['neg']):
+        r['aff'] = do()
+        r['neg'] = do()
+
+    return r
+
 def generate_random_results(round):
     from debate.models import Debate, DebateResult
-    import random
 
     debates = Debate.objects.filter(round=round)
 
     for debate in debates:
         dr = DebateResult(debate)
         
+        rr = gen_results()
         for team in ('aff', 'neg'):
             speakers = getattr(debate, '%s_team' % team).speakers
+            scores = rr[team]
             for i in range(1, 4):
                 dr.set_speaker_entry(
                     team = team,
                     pos = i,
                     speaker = speakers[i - 1],
-                    score = random.randint(60,80)
+                    score = scores[i-1],
                 )
             dr.set_speaker_entry(
                 team = team,
                 pos = 4,
                 speaker = speakers[0],
-                score = random.randint(30,40)
+                score = scores[3]
             )
 
         dr.save()
