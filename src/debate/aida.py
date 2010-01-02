ffd3440711645a59aee2ca2f95fe1a611bc63aa0
Qi-Shan Lim <qishan@bios.net.nz>
2010-01-02 20:46:12 +1300

    fix bug in dp with test case

--- src/debate/aida.py
+++ src/debate/aida.py
@@ -20,10 +20,10 @@ def dp(data):
     #    L.insert(0, j) 
     #    j -= (action[j] + 1)
     #return state[N], L
-    while j >= 0:
-        j -= (action[j] + 1)
+    while j >= 2:
         if action[j]:
             L.insert(0, j-2) # insert index corresponding to start of swap
+        j -= (action[j] + 1)
     return state[N], L
 
 def run():
@@ -81,15 +81,13 @@ def one_up_down(draw):
 from unittest import TestCase
 
 class TestDraw(TestCase):
-    def failUnlessEqual(self, a, b):
-        return
 
     def testNoSwap(self):
         data = (((1, 'A', ()), (5, 'B', ())),
                 ((2, 'C', ()), (6, 'A', ())),
                 ((3, 'B', ()), (7, 'D', ())),
                 ((4, 'C', ()), (8, 'A', ())))
-        result = ((1, 5), (2, 6), (3, 7), (4, 8))
+        result = [(1, 5), (2, 6), (3, 7), (4, 8)]
         self.failUnlessEqual(result, self.draw(data))
         return self.draw(data)
 
@@ -98,7 +96,7 @@ class TestDraw(TestCase):
                 ((2, 'C', ()), (6, 'B', ())),
                 ((3, 'B', ()), (7, 'D', ())),
                 ((4, 'C', ()), (8, 'A', ())))
-        result = ((1, 6), (2, 5), (3, 7), (4, 8))
+        result = [(1, 6), (2, 5), (3, 7), (4, 8)]
         self.failUnlessEqual(result, self.draw(data))
         return self.draw(data)
 
@@ -107,7 +105,16 @@ class TestDraw(TestCase):
                 ((2, 'C', ()), (6, 'A', ())),
                 ((3, 'B', ()), (7, 'D', ())),
                 ((4, 'C', ()), (8, 'A', ())))
-        result = ((1, 6), (2, 5), (3, 7), (4, 8))
+        result = [(1, 6), (2, 5), (3, 7), (4, 8)]
+        self.failUnlessEqual(result, self.draw(data))
+        return self.draw(data)
+
+    def testLastSwap(self):
+        data = (((1, 'A', ()), (5, 'B', ())),
+                ((2, 'C', ()), (6, 'A', ())),
+                ((3, 'B', ()), (7, 'D', ())),
+                ((4, 'C', (8,)), (8, 'A', ())))
+        result = [(1, 5), (2, 6), (3, 8), (4, 7)]
         self.failUnlessEqual(result, self.draw(data))
         return self.draw(data)
 
@@ -117,11 +124,4 @@ class TestDraw(TestCase):
             d.append((Team(p1, in1, hist1), Team(p2, in2, hist2)))
         r = one_up_down(d)
         return [(a.id, b.id) for (a, b) in r]
-
-
-if __name__ == '__main__':
-    run()
-    d = Tests()
-    d.testNoSwap()
-    
 
