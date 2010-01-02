1158ae97d20a745db8c7083cbb2469a6a733ae3e
Qi-Shan Lim <qishan@bios.net.nz>
2010-01-02 20:05:36 +1300

    change nav

--- src/debate/context_processors.py
+++ src/debate/context_processors.py
@@ -1,7 +1,8 @@
-from debate.models import Tournament 
+from debate.models import Tournament, Round 
 
 def debate_context(request):
     return {
         'current_round': Tournament().current_round, 
+        'rounds': Round.objects.all(),
     }
 
