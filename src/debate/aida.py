87f4706b9ee012011aed82a700a58e720a4b7a04
Chuan-Zheng Lee <chuan-zheng.lee@aviatnet.com>
2012-07-06 23:09:31 +1200

    Add comments indicating what dp(data) does

--- src/debate/aida.py
+++ src/debate/aida.py
@@ -3,24 +3,50 @@ INSTITUTION_SCORE = 1
 HISTORY_SCORE = 1e2
 
 def dp(data):
-    """data is a list of integers"""
+    """'data' is a list of integers.
+    Returns a 2-tuple.
+    The second item is the indices of the elements in data that have the maximum
+    possible sum, subject to the constraint that you can't pick two adjacent elements.
+    Note that negative elements will never be included, since they always reduce
+    the sum so it is better to exclude them.  (The constraint does not say that
+    you must pick every other element.) For example, if 'data' is a list of "swap
+    scores", this would return a list of indices of the optimal swap combinations.
+    The first item is the maximum possible sum so obtained.."""
     N = len(data) + 1
+
+    # 'state' is the cumulative sum of the relevant elements.
+    # 'action' elements are 1 if this integer should be included, and 0 if not,
+    # except that a 1 is nullified if the integer immediately following it is
+    # included (i.e. a non-nullified 1).  We keep nullified 1s in the list
+    # because they might become non-nullified again if another 1 is added to
+    # the end of a number of consecutive 1s.
     state = [0] * (N + 1)
     action = [0] * (N + 1)
 
     for i in range(2, N+1):
+        # Bear in mind that, if we "activate" this element, then we must
+        # consequentially exclude the previous element.  Given that, we
+        # would improve the cumulative sum if, and only if, adding this
+        # element to the cumulative sum as of *two* elements ago (to form
+        # the potential cumulative sum of this element) would beat the
+        # cumulative sum as of last element.
         if (state[i-2] + data[i-2]) > state[i-1]:
             action[i] = 1
             state[i] = state[i-2] + data[i-2]
         else:
             action[i] = 0
             state[i] = state[i-1]
+
     j = N
     L = []
     #while j >= 0:
     #    L.insert(0, j)
     #    j -= (action[j] + 1)
     #return state[N], L
+
+    # Now go back through the list starting at the end (since a 1 nullifies
+    # the 1 immediately preceding, assuming the former is not itself
+    # nullified).
     while j >= 2:
         if action[j]:
             L.insert(0, j-2) # insert index corresponding to start of swap
