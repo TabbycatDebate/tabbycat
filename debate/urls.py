b83d37a8d653d2b37957924b3866ff09ad02ebcf
Philip Belesky <contact@philipbelesky.com>
2012-07-08 14:50:41 +1200

    renaming properly

--- debate/urls.py
+++ debate/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'debateo.views.home', name='home'),
-    # url(r'^debateo/', include('debateo.foo.urls')),
+    # url(r'^$', 'debate.views.home', name='home'),
+    # url(r'^debate/', include('debate.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
