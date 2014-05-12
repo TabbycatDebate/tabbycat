796824dac43037d510b8fb4106dae23279cb98b2
Chuan-Zheng Lee <cz.lee11@gmail.com>
2014-05-11 22:24:08 -0700

    Playing with action log scripts

--- src/data/vic2012/add_fake_log.py
+++ src/data/vic2012/add_fake_log.py
@@ -19,10 +19,19 @@ def get_user(username):
 def get_debate(id):
     return m.Debate.objects.get(id=id)
 
+def get_adjudicator_feedback(id):
+    return m.AdjudicatorFeedback.objects.get(id=id)
+
+def get_motion(id):
+    return m.Motion.objects.get(id=id)
+
 #log(type=AL.ACTION_TYPE_BALLOT_CHECKIN, user=get_user('czlee1'))
 #log(type=AL.ACTION_TYPE_BALLOT_CHECKIN, user=get_user('czlee1'), debate=get_debate(145))
 #log(type=AL.ACTION_TYPE_DRAW_CONFIRM, user=get_user('czlee1'), debate=get_debate(145))
 #log(type=AL.ACTION_TYPE_FEEDBACK_SUBMIT, user=get_user('czlee1'), debate=get_debate(145))
+log(type=AL.ACTION_TYPE_FEEDBACK_SUBMIT, user=get_user('notsuper'), adjudicator_feedback=get_adjudicator_feedback(100))
+log(type=AL.ACTION_TYPE_FEEDBACK_SAVE, user=get_user('notsuper'), adjudicator_feedback=get_adjudicator_feedback(138))
+log(type=AL.ACTION_TYPE_MOTION_EDIT, user=get_user('ravi'), motion=get_motion(43))
 
 for al in m.ActionLog.objects.all():
     print repr(al)
--- src/data/vic2012/read_log.py
+++ src/data/vic2012/read_log.py
@@ -2,3 +2,7 @@ import debate.models as m
 
 for al in m.ActionLog.objects.all():
     print repr(al)
+    for field in m.ActionLog.ALL_OPTIONAL_FIELDS:
+        field_value = getattr(al, field)
+        if field_value:
+            print "    ", field, getattr(al, field)
