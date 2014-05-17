4b661b4e67bb4f7b64ed2235d129016e46a226b4
Philip Belesky <contact@philipbelesky.com>
2014-05-18 09:55:21 +1000

    db settings tinkering

--- settings.py
+++ settings.py
@@ -95,7 +95,7 @@ if 'HEROKU' in os.environ:
     DEBUG = True
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
-    DATABASES['default'] = dj_database_url.config(default='postgres://localhost')
+    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     # Allow all host headers
