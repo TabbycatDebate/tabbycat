4a30daf021fefdd80a45c8bb51ce70410f527721
Philip Belesky <contact@philipbelesky.com>
2014-05-18 09:48:47 +1000

    db settings tinkering

--- settings.py
+++ settings.py
@@ -92,10 +92,12 @@ except Exception as e:
     pass
 
 if 'HEROKU' in os.environ:
-    import dj_database_url
     DEBUG = True
     # Parse database configuration from $DATABASE_URL
-    DATABASES['default'] =  dj_database_url.config()
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
 
