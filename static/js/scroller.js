6198245127301831bb8e940b72d93ce7a7ac860f
Philip Belesky <contact@philipbelesky.com>
2014-05-30 23:59:16 +1000

    Sundry style changes

--- static/js/scroller.js
+++ static/js/scroller.js
@@ -1,14 +1,17 @@
 $(document).ready( function() {
     $("#scroll_draw1").click(function(event){
-        $('html, body').animate({ scrollTop: $(document).height() - $(window).height()}, 40000);
+        $('html, body').animate({
+            scrollTop: $(document).height() - $(window).height()}, 20000, "linear");
         return false;
     });
     $("#scroll_draw2").click(function(event){
-        $('html, body').animate({ scrollTop: $(document).height() - $(window).height()}, 80000);
+        $('html, body').animate({
+            scrollTop: $(document).height() - $(window).height()}, 40000, "linear");
         return false;
     });
     $("#scroll_draw3").click(function(event){
-        $('html, body').animate({ scrollTop: $(document).height() - $(window).height()}, 120000);
+        $('html, body').animate({
+            scrollTop: $(document).height() - $(window).height()}, 60000, "linear");
         return false;
     });
 });
