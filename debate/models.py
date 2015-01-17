50ab4fcc8bd8e240e9ae028505293f113346a448
Philip Belesky <contact@philipbelesky.com>
2015-01-17 18:04:20 +1100

    Updated requirements, removing South introspectiono

--- debate/models.py
+++ debate/models.py
@@ -17,8 +17,9 @@ from collections import OrderedDict
 class ScoreField(models.FloatField):
     pass
 
-from south.modelsinspector import add_introspection_rules
-add_introspection_rules([], ["^debate\.models\.ScoreField"])
+#Commented out - potential bug source for testing if core migrations doesn't account for this
+#from south.modelsinspector import add_introspection_rules
+#add_introspection_rules([], ["^debate\.models\.ScoreField"])
 
 
 class Tournament(models.Model):
