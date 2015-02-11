95661b0e8a43aa6f2e54dec0da5b99cfd248134d
Philip Belesky <contact@philipbelesky.com>
2015-02-11 20:11:25 +1100

    Enginer fix

--- settings.py
+++ settings.py
@@ -123,11 +123,11 @@ if 'HEROKU' in os.environ:
     import dj_database_url
 
     DATABASES['default'] =  dj_database_url.config()
+    DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     # Allow all host headers
     ALLOWED_HOSTS = ['*']
-
 
 # Local
 try:
