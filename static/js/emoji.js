37887a96765e48649d34925aca8d740048ee2288
Philip Belesky <contact@philipbelesky.com>
2015-02-19 18:53:02 +1100

    Fix for emoji on participants page, added a no motions stage to public motions page

--- static/js/emoji.js
+++ static/js/emoji.js
@@ -12,7 +12,8 @@ $(document).ready( function() {
             team_ids.push(ID); // building an array of team IDS
         });
         team_ids.sort(function(a,b){return a - b})
-        console.log(team_ids.length);
+        console.log(team_ids.length + ' to make emoji');
+        console.log(team_ids);
         for (var i = 0; i < team_ids.length; i++) {
             var base_id = team_ids[i] - team_ids[0] // Starting from Emoji[0]
             $(".team-" + team_ids[i]).html('<img src="' + emoji_arr[base_id] + '" />');
