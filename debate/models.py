cf459163f86a462eb3b11ad08547f3d96ae0ecf7
Philip Belesky <contact@philipbelesky.com>
2015-02-28 10:40:10 +1100

    Data import fixes

--- debate/models.py
+++ debate/models.py
@@ -99,7 +99,7 @@ class VenueGroup(models.Model):
     name = models.CharField(max_length=200)
     short_name = models.CharField(max_length=25, blank=True, null=True, default="")
     tournament = models.ForeignKey(Tournament)
-    team_capacity = models.IntegerField()
+    team_capacity = models.IntegerField(blank=True, null=True)
 
     @property
     def venues(self):
