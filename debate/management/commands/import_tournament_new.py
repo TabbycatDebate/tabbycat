3c76105c426bfd7daeebdccd24fc6cdfaffd1092
Chuan-Zheng Lee <czlee@stanford.edu>
2015-03-03 18:51:28 -0800

    Work in progress

--- debate/management/commands/import_tournament_new.py
+++ debate/management/commands/import_tournament_new.py
@@ -6,27 +6,35 @@ import csv
 import debate.models as m
 from optparse import make_option
 
-class Command(LabelCommand):
-    args = '<dirname dirname ...> [--auto-rounds n] [--share-data]'
+class Command(BaseCommand):
+    args = 'path [--auto-rounds n] [--share-data] [--slug SLUG] [--name NAME] [--short-name NAME]'
     help = 'Delete all data for a tournament and import from specified directory.'
 
-    option_list = LabelCommand.option_list + (
+    option_list = BaseCommand.option_list + (
         make_option('-r', '--auto-rounds', type=int, metavar='N', default=None,
             help='Create N preliminary rounds automatically. Use either this or a rounds.csv file, but not both.'),
         make_option('-S', '--share-data', action='store_true', default=False,
             help='If specified, all institutions and adjudicators will not be tournament-specific.'),
         make_option("--force", action='store_true', default=False,
-            help='Delete tournaments if they already exist.')
+            help='Delete tournaments if they already exist.'),
+        make_option('-s', '--slug', type=str, action='store', default=None,
+            help='Override tournament slug.'),
+        make_option('--name', type=str, action='store', default=None,
+            help='Override tournament name.'),
+        make_option('--short-name', type=str, action='store', default=None,
+            help='Override tournament short name.'),
     )
 
     def _print_stage(self, message):
         if self.verbosity > 0:
             print("\e[1;36m" + message + "\e[0m")
 
-    def handle_label(self, label, **options):
+    def handle(self, *args, **options):
         self.verbosity = options['verbosity']
         self.check_if_tournament_exists(label, options['force'])
         dirname = self.get_dir(self, label)
+        tslug = slugify(unicode(os.path.basename(dirname.rstrip('/'))))
+
 
     def get_dir(self, label):
         """Returns the directory for the given label. If the label is an
@@ -40,7 +48,13 @@ class Command(LabelCommand):
         data_path = os.path.join(base_path, label)
         return data_path
 
+    def resolve_tournament_fields(self):
+        pass
+
     def check_if_tournament_exists(self, label, force=False):
+        """Checks if a tournament exists."""
+        if force:
+            return
         slug = slugify(unicode(label))
         if m.Tournament.objects.filter(slug=slug).exists():
             self.stdout.write("WARNING! A tournament called '" + label + "' already exists.")
@@ -51,6 +65,14 @@ class Command(LabelCommand):
                 raise CommandError("Cancelled by user.")
             m.Tournament.objects.filter(slug=slug).delete()
 
-    def create_tournament(self, label):
+    def create_tournament(self, slug):
+        """Creates, saves and returns a tournament with the given label.
+        Raises exception on error."""
         self._print_stage("Creating tournament '" + label + "'")
-        slug = slugify(unicode(label))
+        short_name = (label[:24] + '..') if len(label) > 75 else label
+        t = m.Tournament(name=label, short_name=short_name, slug=slug)
+        t.save()
+        return t
+
+    def create_rounds_from_file(self, dirname):
+        pass
