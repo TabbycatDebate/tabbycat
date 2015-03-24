992f57e17de9e784c0c284486573a1667a98e119
Philip Belesky <contact@philipbelesky.com>
2015-03-24 22:40:38 +1100

    Emoji refactor working concept
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

---
+++ debate/emoji.py
@@ -0,0 +1,113 @@
+EMOJI_LIST = [
+    '\xF0\x9F\x98\x80', # grinning face
+    '\xF0\x9F\x9A\x8A', # tram
+    '\xF0\x9F\x98\x87', # smiling face with halo
+    '\xF0\x9F\x9B\x81', # bathtub
+    '\xF0\x9F\x98\x88', # smiling face with horns
+    '\xF0\x9F\x8D\x8B', # lemon
+    '\xF0\x9F\x8C\x9E', # sun with face
+    '\xF0\x9F\x9A\x9D', # monorail
+    '\xF0\x9F\x94\xAD', # telescope
+    '\xF0\x9F\x98\x8E', # smiling face with sunglasses
+    '\xF0\x9F\x8F\x89', # rugby football
+    '\xF0\x9F\x8C\x9A', # new moon with face
+    '\xF0\x9F\x90\x90', # goat
+    '\xF0\x9F\x98\x90', # neutral face
+    '\xF0\x9F\x9A\xB4', # bicyclist
+    '\xF0\x9F\x98\x91', # expressionless face
+    '\xF0\x9F\x90\x88', # cat
+    '\xF0\x9F\x98\x95', # confused face
+    '\xF0\x9F\x98\x97', # kissing face
+    '\xF0\x9F\x98\x99', # kissing face with smiling eyes
+    '\xF0\x9F\x98\x9B', # face with stuck-out tongue
+    '\xF0\x9F\x98\x9F', # worried face
+    '\xF0\x9F\x98\xA6', # frowning face with open mouth
+    '\xF0\x9F\x98\xA7', # anguished face
+    '\xF0\x9F\x98\xAC', # grimacing face
+    '\xF0\x9F\x98\xAE', # face with open mouth
+    '\xF0\x9F\x98\xAF', # hushed face
+    '\xF0\x9F\x98\xB4', # sleeping face
+    '\xF0\x9F\x98\xB6', # face without mouth
+    '\xF0\x9F\x9A\x81', # helicopter
+    '\xF0\x9F\x9A\x82', # steam locomotive
+    '\xF0\x9F\x9A\x86', # train
+    '\xF0\x9F\x9A\x88', # light rail
+    '\xF0\x9F\x9A\x8D', # oncoming bus
+    '\xF0\x9F\x9A\x8E', # trolleybus
+    '\xF0\x9F\x9A\x90', # minibus
+    '\xF0\x9F\x9A\x94', # oncoming police car
+    '\xF0\x9F\x9A\x96', # oncoming taxi
+    '\xF0\x9F\x9A\x98', # oncoming automobile
+    '\xF0\x9F\x9A\x9B', # articulated lorry
+    '\xF0\x9F\x9A\x9C', # tractor
+    '\xF0\x9F\x9A\x9E', # mountain railway
+    '\xF0\x9F\x9A\x9F', # suspension railway
+    '\xF0\x9F\x9A\xA0', # mountain cableway
+    '\xF0\x9F\x9A\xA1', # aerial tramway
+    '\xF0\x9F\x9A\xA3', # rowboat
+    '\xF0\x9F\x9A\xA6', # vertical traffic light
+    '\xF0\x9F\x9A\xAE', # put litter in its place symbol
+    '\xF0\x9F\x9A\xAF', # do not litter symbol
+    '\xF0\x9F\x9A\xB0', # potable water symbol
+    '\xF0\x9F\x9A\xB1', # non-potable water symbol
+    '\xF0\x9F\x9A\xB3', # no bicycles
+    '\xF0\x9F\x9A\xB5', # mountain bicyclist
+    '\xF0\x9F\x9A\xB7', # no pedestrians
+    '\xF0\x9F\x9A\xB8', # children crossing
+    '\xF0\x9F\x9A\xBF', # shower
+    '\xF0\x9F\x9B\x82', # passport control
+    '\xF0\x9F\x9B\x83', # customs
+    '\xF0\x9F\x9B\x84', # baggage claim
+    '\xF0\x9F\x9B\x85', # left luggage
+    '\xF0\x9F\x8C\x8D', # earth globe europe-africa
+    '\xF0\x9F\x8C\x8E', # earth globe americas
+    '\xF0\x9F\x8C\x90', # globe with meridians
+    '\xF0\x9F\x8C\x92', # waxing crescent moon symbol
+    '\xF0\x9F\x8C\x96', # waning gibbous moon symbol
+    '\xF0\x9F\x8C\x97', # last quarter moon symbol
+    '\xF0\x9F\x8C\x98', # waning crescent moon symbol
+    '\xF0\x9F\x8C\x9C', # last quarter moon with face
+    '\xF0\x9F\x8C\x9D', # full moon with face
+    '\xF0\x9F\x8C\xB2', # evergreen tree
+    '\xF0\x9F\x8C\xB3', # deciduous tree
+    '\xF0\x9F\x8D\x90', # pear
+    '\xF0\x9F\x8D\xBC', # baby bottle
+    '\xF0\x9F\x8F\x87', # horse racing
+    '\xF0\x9F\x8F\xA4', # european post office
+    '\xF0\x9F\x90\x80', # rat
+    '\xF0\x9F\x90\x81', # mouse
+    '\xF0\x9F\x90\x82', # ox
+    '\xF0\x9F\x90\x83', # water buffalo
+    '\xF0\x9F\x90\x84', # cow
+    '\xF0\x9F\x90\x85', # tiger
+    '\xF0\x9F\x90\x86', # leopard
+    '\xF0\x9F\x90\x87', # rabbit
+    '\xF0\x9F\x90\x89', # dragon
+    '\xF0\x9F\x90\x8A', # crocodile
+    '\xF0\x9F\x90\x8B', # whale
+    '\xF0\x9F\x90\x8F', # ram
+    '\xF0\x9F\x90\x93', # rooster
+    '\xF0\x9F\x90\x95', # dog
+    '\xF0\x9F\x90\x96', # pig
+    '\xF0\x9F\x90\xAA', # dromedary camel
+    '\xF0\x9F\x91\xA5', # busts in silhouette
+    '\xF0\x9F\x91\xAC', # two men holding hands
+    '\xF0\x9F\x91\xAD', # two women holding hands
+    '\xF0\x9F\x92\xAD', # thought balloon
+    '\xF0\x9F\x92\xB6', # banknote with euro sign
+    '\xF0\x9F\x92\xB7', # banknote with pound sign
+    '\xF0\x9F\x93\xAC', # open mailbox with raised flag
+    '\xF0\x9F\x93\xAD', # open mailbox with lowered flag
+    '\xF0\x9F\x93\xAF', # postal horn
+    '\xF0\x9F\x93\xB5', # no mobile phones
+    '\xF0\x9F\x94\x80', # twisted rightwards arrows
+    '\xF0\x9F\x94\x81', # clockwise rightwards and leftwards open circle arrows
+    '\xF0\x9F\x94\x82', # clockwise rightwards and leftwards open circle arrows with circled one overlay
+    '\xF0\x9F\x94\x84', # anticlockwise downwards and upwards open circle arrows
+    '\xF0\x9F\x94\x85', # low brightness symbol
+    '\xF0\x9F\x94\x86', # high brightness symbol
+    '\xF0\x9F\x94\x87', # speaker with cancellation stroke
+    '\xF0\x9F\x94\x89', # speaker with one sound wave
+    '\xF0\x9F\x94\x95', # bell with cancellation stroke
+    '\xF0\x9F\x94\xAC', # microscope
+]
