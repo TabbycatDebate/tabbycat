4d4b94414397d0a9ce6f768391fe1c941045997f
Philip Belesky <contact@philipbelesky.com>
2015-03-25 14:38:43 +1100

    Moved datasets to the submodule format, removed default data sets
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- data/vic2012/read_log.py
+++
@@ -1,8 +0,0 @@
-import debate.models as m
-
-for al in m.ActionLog.objects.all():
-    print repr(al)
-    for field in m.ActionLog.ALL_OPTIONAL_FIELDS:
-        field_value = getattr(al, field)
-        if field_value:
-            print "    ", field, getattr(al, field)
