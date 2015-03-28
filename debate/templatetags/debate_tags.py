dcb447046e527c9ee02d66715df210c61a4852f8
Philip Belesky <contact@philipbelesky.com>
2015-03-28 12:37:33 +1100

    Printable forms development
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- debate/templatetags/debate_tags.py
+++ debate/templatetags/debate_tags.py
@@ -48,9 +48,6 @@ def team_emoji(team):
         return b"%s" % EMOJI_LIST[team.emoji_seq]
     else:
         if team.id > len(EMOJI_LIST):
-            print len(EMOJI_LIST)
-            print team.id
-            print team.id % len(EMOJI_LIST)
             return b"%s" % EMOJI_LIST[(team.id % len(EMOJI_LIST))] # Wrapping around if too high
         else:
             return b"%s" % EMOJI_LIST[team.id]
