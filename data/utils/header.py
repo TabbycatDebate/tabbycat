fa1169a39e00eb7c462e3b1267eb77f7dffad3da
Chuan-Zheng Lee <czlee@stanford.edu>
2015-05-25 11:41:02 -0700

    Update header.py

--- data/utils/header.py
+++ data/utils/header.py
@@ -5,8 +5,9 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 import sys
 venv = os.environ.get("VIRTUAL_ENV")
-if venv is None:
-    print("You must be in the virtual environment to run this script.")
+pyhome = os.environ.get("PYTHONHOME", "")
+if venv is None and "heroku" not in pyhome:
+    print("You must be in the virtual environment or Heroku to run this script.")
     exit()
 root_path = os.path.abspath(os.path.join(venv, ".."))
 if root_path not in sys.path: sys.path.append(root_path)
