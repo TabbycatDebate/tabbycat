7ead05a9d777a00260685512eddbd81044c51c81
Chuan-Zheng Lee <czlee@stanford.edu>
2015-05-25 11:45:26 -0700

    Update header.py

--- data/utils/header.py
+++ data/utils/header.py
@@ -6,10 +6,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 import sys
 venv = os.environ.get("VIRTUAL_ENV")
 pyhome = os.environ.get("PYTHONHOME", "")
-if venv is None and "heroku" not in pyhome:
+if venv is not None:
+    root_path = os.path.abspath(os.path.join(venv, ".."))
+elif "heroku" in pyhome:
+    root_path = os.environ.get("PYTHONPATH")
+else:
     print("You must be in the virtual environment or Heroku to run this script.")
     exit()
-root_path = os.path.abspath(os.path.join(venv, ".."))
 if root_path not in sys.path: sys.path.append(root_path)
 import django
 django.setup()
