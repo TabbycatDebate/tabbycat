20d9aa8dd4e8e6dcc2a7f63ee99c955e83b32e79
Chuan-Zheng Lee <czlee@stanford.edu>
2015-06-23 16:01:07 -0700

    Fix for Windows

--- deploy_heroku.py
+++ deploy_heroku.py
@@ -4,6 +4,7 @@ import argparse
 import subprocess
 import re
 import sys
+import platform
 
 parser = argparse.ArgumentParser(description="Deploy Tabbycat to a new Heroku app.")
 parser.add_argument("urlname", type=str, help="Name of the Heroku app. The app will be at urlname.herokuapp.com. Use '-' to use a Heroku-generated default.")
@@ -16,17 +17,29 @@ parser.add_argument("--import-tournament", type=str, help="Also run the import_t
 parser.add_argument("--git-remote", type=str, help="Name of Git remote to use", default=None)
 args = parser.parse_args()
 
+if platform.system() == "Windows":
+    subprocess_kwargs = dict(shell=True)
+else:
+    subprocess_kwargs = dict()
+
 def print_command(command):
     print "\033[1;36m $ " + " ".join(command) + "\033[0m"
 
 def run_command(command):
     print_command(command)
-    subprocess.check_call(command)
+    subprocess.check_call(command, **subprocess_kwargs)
 
 def run_heroku_command(command):
     command.insert(0, "heroku")
     command.extend(["--app", urlname])
     run_command(command)
+
+def get_output_from_command(command):
+    print_command(command)
+    output = subprocess.check_output(command, **subprocess_kwargs)
+    sys.stdout.write(output)
+    sys.stdout.flush()
+    return output
 
 def print_yellow(message):
     print "\033[1;33m" + message + "\033[0m"
@@ -35,10 +48,7 @@ def print_yellow(message):
 command = ["heroku", "apps:create"]
 if args.urlname != "-":
     command.append(args.urlname)
-print_command(command)
-output = subprocess.check_output(command)
-sys.stdout.write(output)
-sys.stdout.flush()
+output = get_output_from_command(command)
 match = re.search("https://([\w_-]+)\.herokuapp\.com/\s+\|\s+(https://git.heroku.com/[\w_-]+.git)", output)
 urlname = match.group(1)
 heroku_url = match.group(2)
