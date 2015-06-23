6d7db08a2525f73d94739d7f6e1a4c449f40a8dd
Chuan-Zheng Lee <czlee@stanford.edu>
2015-06-23 15:31:24 -0700

    Work on deploy_heroku.py

--- deploy_heroku.py
+++ deploy_heroku.py
@@ -3,6 +3,7 @@
 import argparse
 import subprocess
 import re
+import sys
 
 parser = argparse.ArgumentParser(description="Deploy Tabbycat to a new Heroku app.")
 parser.add_argument("urlname", type=str, help="Name of the Heroku app. The app will be at urlname.herokuapp.com. Use '-' to use a Heroku-generated default.")
@@ -10,8 +11,9 @@ parser.add_argument("--public-cache-timeout", type=int, default=None, help="Set 
 parser.add_argument("--tab-cache-timeout", type=int, default=None, help="Set the tab page cache timeout to TIMEOUT", metavar="TIMEOUT")
 parser.add_argument("--enable-debug", action="store_true", default=False, help="Enable Django debug pages")
 parser.add_argument("--no-open", action="store_false", default=True, dest="open", help="Don't open the Heroku website in your browser at the end")
+parser.add_argument("--no-init-db", action="store_false", default=True, dest="init_db", help="Don't run initial migrations on the database")
 parser.add_argument("--import-tournament", type=str, help="Also run the import_tournament command, importing from IMPORT_DIR", metavar="IMPORT_DIR")
-parser.add_argument("--git-remote-name", type=str, help="Name of Git remote to use", default=None)
+parser.add_argument("--git-remote", type=str, help="Name of Git remote to use", default=None)
 args = parser.parse_args()
 
 def print_command(command):
@@ -27,7 +29,7 @@ def run_heroku_command(command):
     run_command(command)
 
 def print_yellow(message):
-    print "\033[1;32m" + message + "\033[0m"
+    print "\033[1;33m" + message + "\033[0m"
 
 # Create the app
 command = ["heroku", "apps:create"]
@@ -35,7 +37,8 @@ if args.urlname != "-":
     command.append(args.urlname)
 print_command(command)
 output = subprocess.check_output(command)
-print output
+sys.stdout.write(output)
+sys.stdout.flush()
 match = re.search("https://([\w_-]+)\.herokuapp\.com/\s+\|\s+(https://git.heroku.com/[\w_-]+.git)", output)
 urlname = match.group(1)
 heroku_url = match.group(2)
@@ -53,23 +56,30 @@ if args.tab_cache_timeout:
 run_heroku_command(command)
 
 # Set up a remote, if applicable
-remote_name = args.git_remote_name or urlname
-run_command(["git", "remote", "add", remote_name, heroku_url])
+if args.git_remote:
+    remote_name = args.git_remote
+    run_command(["git", "remote", "add", remote_name, heroku_url])
+else:
+    remote_name = heroku_url
 
 # Push source code to Heroku
 run_command(["git", "push", remote_name, "master"])
 
-run_heroku_command(["run", "python", "manage.py", "migrate", "auth"])
-run_heroku_command(["run", "python", "manage.py", "migrate"])
-run_heroku_command(["run", "python", "manage.py", "makemigrations", "debate"])
-run_heroku_command(["run", "python", "manage.py", "migrate"])
+if args.init_db:
+    run_heroku_command(["run", "python", "manage.py", "migrate", "auth"])
+    run_heroku_command(["run", "python", "manage.py", "migrate"])
+    run_heroku_command(["run", "python", "manage.py", "makemigrations", "debate"])
+    run_heroku_command(["run", "python", "manage.py", "migrate"])
 
-print_yellow("Now creating a superuser for the Heroku site.")
-print_yellow("You'll need to respond to the prompts:")
-run_heroku_command(["run", "python", "manage.py", "createsuperuser"])
+    print_yellow("Now creating a superuser for the Heroku site.")
+    print_yellow("You'll need to respond to the prompts:")
+    run_heroku_command(["run", "python", "manage.py", "createsuperuser"])
 
 if args.import_tournament:
-    run_heroku_command(["run", "python", "manage.py", "import_tournament", args.import_tournament])
+    if args.init_db:
+        run_heroku_command(["run", "python", "manage.py", "import_tournament", args.import_tournament])
+    else:
+        print_yellow("Warning: You can't use --import-tournament when --no-init-db is used.")
 
 if args.open:
     run_heroku_command(["open"])
