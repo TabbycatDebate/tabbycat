cba2e5354ea67cf19b3c4d1c0bcd4084b74caf69
Philip Belesky <contact@philipbelesky.com>
2015-07-18 16:32:35 +0900

    Update to feedback model length; slight tweak to printed scoresheets
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- debate/models.py
+++ debate/models.py
@@ -1492,7 +1492,7 @@ class AdjudicatorFeedbackFloatAnswer(AdjudicatorFeedbackAnswer):
     answer = models.FloatField()
 
 class AdjudicatorFeedbackStringAnswer(AdjudicatorFeedbackAnswer):
-    answer = models.CharField(max_length=500)
+    answer = models.CharField(max_length=3000)
 
 
 class AdjudicatorFeedbackQuestion(models.Model):
