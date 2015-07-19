eb64c63d68b4204ecb9c2a6de44df4d6d26dba27
Philip Belesky <contact@philipbelesky.com>
2015-07-19 14:57:36 +0900

    Removing django debug on live
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- settings.py
+++ settings.py
@@ -172,48 +172,6 @@ if os.environ.get('DEBUG', ''):
     DEBUG = bool(int(os.environ['DEBUG']))
     TEMPLATE_DEBUG = DEBUG
 
-# =========
-# = DEBUG TOOLBAR ON LIVE =
-# =========
-
-def show_toolbar(request):
-    return not request.is_ajax() and request.user and request.user.is_superuser
-
-if os.environ.get('DEBUGTOOLBAR', ''):
-
-    MIDDLEWARE_CLASSES += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-    )
-
-    INSTALLED_APPS += (
-        'debug_toolbar',
-    )
-
-    DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.versions.VersionsPanel',
-        'debug_toolbar.panels.timer.TimerPanel',
-        'debug_toolbar.panels.settings.SettingsPanel',
-        'debug_toolbar.panels.headers.HeadersPanel',
-        'debug_toolbar.panels.request.RequestPanel',
-        'debug_toolbar.panels.sql.SQLPanel',
-        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-        'debug_toolbar.panels.templates.TemplatesPanel',
-        'debug_toolbar.panels.cache.CachePanel',
-        'debug_toolbar.panels.signals.SignalsPanel',
-        'debug_toolbar.panels.logging.LoggingPanel',
-        'debug_toolbar.panels.redirects.RedirectsPanel',
-    )
-
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
-    }
-
-    DEBUG_TOOLBAR_CONFIG = {
-        'SHOW_TOOLBAR_CALLBACK': 'settings.show_toolbar',
-    }
-
 # ===========================
 # = Local Overrides
 # ===========================
