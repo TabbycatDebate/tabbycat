648ef1b248bdfd25be8cde0c1c456f1261647d7c
Chuan-Zheng Lee <czlee@stanford.edu>
2015-07-21 19:39:20 +1200

    Add clear_cache command

---
+++ debate/management/commands/clear_cache.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.urlresolvers import reverse
+from django.utils.cache import get_cache_key
+from django.core.cache import cache
+from django.test import RequestFactory
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument("path")
+
+    def handle(self, *args, **options):
+        path = options['path']
+        factory = RequestFactory()
+        request = factory.get(path)
+        key = get_cache_key(request, key_prefix=None)
+        if not key:
+            self.stdout.write("Could not find key for " + str(path) + "\n")
+            return
+        if not cache.get(key):
+            self.stdout.write("Could not find key " + str(key) + "\n")
+            return
+        if key and cache.get(key):
+            cache.delete(key)
+            self.stdout.write("Deleted cache for key " + str(key) + "\n")
