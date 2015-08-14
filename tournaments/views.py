b8021aaaf82992b11bfe90f5509fa2e4f7b548ac
Philip Belesky <contact@philipbelesky.com>
2015-08-14 13:46:12 +1000

    Removing duplicate views
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- tournaments/views.py
+++ tournaments/views.py
@@ -36,13 +36,6 @@ def tournament_home(request, t):
     return r2r(request, 'tournament_home.html', dict(stats=stats,
         total_ballots=total_ballots, round=round))
 
-
-@cache_page(10) # Set slower to show new indexes so it will show new pages
-@tournament_view
-def public_index(request, t):
-    return r2r(request, 'public_tournament_index.html')
-
-
 @cache_page(settings.PUBLIC_PAGE_CACHE_TIMEOUT)
 @public_optional_tournament_view('public_divisions')
 def public_divisions(request, t):
@@ -85,32 +78,6 @@ def all_draws_for_institution(request, t, institution_id):
     return r2r(request, 'public_all_draws_for_institution.html', dict(
         institution=institution, debates=debates))
 
-
-## Tab
-
-@login_required
-@tournament_view
-def tournament_home(request, t):
-    round = t.current_round
-    # This should never happen, but if it does, fail semi-gracefully
-    if round is None:
-        if request.user.is_superuser:
-            return HttpResponseBadRequest("You need to set the current round. <a href=\"/admin/debate/tournament\">Go to Django admin.</a>")
-        else:
-            raise Http404()
-
-    rounds = t.prelim_rounds(until=round).order_by('seq')
-
-    # Draw Status
-    draw = round.get_draw()
-    total_ballots = draw.count()
-    stats_none = draw.filter(result_status=Debate.STATUS_NONE).count()
-    stats_draft = draw.filter(result_status=Debate.STATUS_DRAFT).count()
-    stats_confirmed = draw.filter(result_status=Debate.STATUS_CONFIRMED).count()
-    stats = [[0,stats_confirmed], [0,stats_draft], [0,stats_none]]
-
-    return r2r(request, 'tournament_home.html', dict(stats=stats,
-        total_ballots=total_ballots, round=round))
 
 
 @admin_required
