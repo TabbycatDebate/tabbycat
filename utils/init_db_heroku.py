f8b6fb466804565d78b9eaf0fb3bf4141188e047
Chuan-Zheng Lee <czlee@stanford.edu>
2015-08-23 23:56:49 -0700

    Update init_db_heroku.py script

--- utils/init_db_heroku.py
+++ utils/init_db_heroku.py
@@ -18,5 +18,5 @@ def run_command(command):
     print_command(command)
     subprocess.check_call(command, **subprocess_kwargs)
 
-run_command(["python", "manage.py", "makemigrations", "debate"])
+run_command(["python", "manage.py", "makemigrations", "actionlog", "adjallocation", "adjfeedback", "availability", "breakqual", "draw", "motions", "options", "participants", "results", "tournaments", "venues"])
 run_command(["python", "manage.py", "migrate"])
