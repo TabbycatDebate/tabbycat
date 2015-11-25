3cfc617fdd122d26ed6af9b2a0a45b3222f7b1d8
Philip Belesky <contact@philipbelesky.com>
2015-11-26 00:39:50 +1100

    Wiring up the data imports
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- importer/views.py
+++ importer/views.py
@@ -19,7 +19,7 @@ def add_institutions(request, t):
 @admin_required
 @expect_post
 @tournament_view
-def confirm_institutions(request, t):
+def edit_institutions(request, t):
     institutions = []
     institution_lines = request.POST['institutions_raw'].split('\n')
     for line in institution_lines:
@@ -31,7 +31,27 @@ def confirm_institutions(request, t):
         except:
             pass # TODO
 
-    return r2r(request, 'confirm_institutions.html', dict(institutions=institutions))
+    return r2r(request, 'edit_institutions.html', dict(institutions=institutions))
+
+
+@admin_required
+@expect_post
+@tournament_view
+def confirm_institutions(request, t):
+    institution_names = request.POST.getlist('institution_names')
+    institution_codes = request.POST.getlist('institution_codes')
+
+    for i, key in enumerate(institution_names):
+        try:
+            full_name = institution_names[i]
+            short_name = institution_codes[i]
+            institution = Institution(name=full_name, code=short_name)
+            institution.save()
+        except:
+            pass
+
+    confirmed = {"kind": "Institutions", "quantity": len(institution_names) }
+    return r2r(request, 'confirmed_data.html', dict(confirmed=confirmed))
 
 @admin_required
 @tournament_view
@@ -45,6 +65,43 @@ def add_teams(request, t):
     institutions = Institution.objects.all()
     form = forms.AddTeamsForm
     return r2r(request, 'add_teams.html', dict(institutions=institutions))
+
+
+@admin_required
+@expect_post
+@tournament_view
+def edit_teams(request, t):
+    institutions = {}
+    for name, quantity in request.POST.items():
+        if quantity:
+            institutions[name] = list(range(1, int(quantity) + 1)) # Create a placeholder for loop
+
+    return r2r(request, 'edit_teams.html', dict(institutions=institutions))
+
+
+@admin_required
+@expect_post
+@tournament_view
+def confirm_teams(request, t):
+
+    sorted_post = sorted(request.POST.items())
+    for i in range(0, len(sorted_post), 2):
+        name = sorted_post[i][1]
+        speakers = sorted_post[i+1][1].split(',')
+        if name and speakers:
+            # TODO: create team name and speakers now
+            team = Team(
+                institution = bye_institution,
+                reference = name,
+                short_reference = name[:34],
+                tournament= this,
+                use_institution_prefix = False,
+            ).save()
+            for speaker in speakers:
+                print(speaker)
+
+    confirmed = {"kind": "Teams", "quantity": 11 }
+    return r2r(request, 'confirmed_data.html', dict(confirmed=confirmed))
 
 @admin_required
 @tournament_view
