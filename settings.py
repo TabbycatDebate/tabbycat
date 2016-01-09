35d620768925d0c731069145ae18373ab42184eb
Philip Belesky <contact@philipbelesky.com>
2016-01-09 16:22:21 +1100

    Added a few more emojii; updated local settings to prevent caching templates
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- settings.py
+++ settings.py
@@ -99,9 +99,9 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
-                'django.template.context_processors.request',  # For SUIT
-                'utils.context_processors.debate_context', # For tournament config vars
-                'utils.context_processors.get_menu_highlight', # For nav highlights
+                'django.template.context_processors.request',   # For SUIT
+                'utils.context_processors.debate_context',      # For tournament config vars
+                'utils.context_processors.get_menu_highlight',  # For nav highlights
             ],
             'loaders': [
                 ('django.template.loaders.cached.Loader', [
