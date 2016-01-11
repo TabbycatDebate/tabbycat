6a88c4a2835746373154f57e9b1144b545e2cf2b
Philip Belesky <contact@philipbelesky.com>
2016-01-11 11:10:19 +1100

    Slight update to serializers
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- options/forms.py
+++ options/forms.py
@@ -7,15 +7,15 @@ class FloatSerializer(BaseSerializer):
     @classmethod
     def clean_to_db_value(cls, value):
         if not isinstance(value, float):
-            raise cls.exception('FloatSerializer can only serialize float values')
+            raise cls.exception('FloatSerializer can only serialize Float instances')
         return value
 
     @classmethod
     def to_python(cls, value, **kwargs):
         try:
             return float(value)
-        except:
-            raise cls.exception("Value {0} cannot be converted to a float")
+        except float.InvalidOperation:
+            raise cls.exception("Value {0} cannot be converted to float".format(value))
 
 
 class FloatPreference(BasePreferenceType):
