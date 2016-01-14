89f02ab9883e074ff903e7b1c1a25b776febdbe2
Philip Belesky <contact@philipbelesky.com>
2016-01-14 20:26:29 +1100

    Adding email for debug logs
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ SECRET_KEY          = '#2q43u&tp4((4&m3i8v%w-6z6pp7m(v0-6@w@i!j5n)n15epwc'
 # = Overwritten in Local =
 # ===================
 
-ADMINS              = ('Test', 'test@test.com')
+ADMINS              = ('Philip and CZ', 'tabbycat@philipbelesky.com')
 MANAGERS            = ADMINS
 DEBUG               = False
 TEMPLATE_DEBUG      = DEBUG
