014eaf51da7e1c8dd330ca9c2a687acb6bd17f9d
Philip Belesky <contact@philipbelesky.com>
2016-01-27 22:20:17 +1100

    Added emoji field to tournament, updated template
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- participants/emoji.py
+++ participants/emoji.py
@@ -1,139 +1,139 @@
 # -*- coding: utf-8 -*-
 
-EMOJI_LIST = [
-    "😁", # grinning face with smiling eyes
-    "😂", # face with tears of joy
-    "😃", # smiling face with open mouth
-    "😉", # winking face
-    "😍", # smiling face with heart-shaped eyes
-    "😏", # smirking face
-    "😘", # face throwing a kiss
-    "😡", # pouting face
-    "😱", # face screaming in fear
-    "😵", # dizzy face
-    "😻", # smiling cat face with heart-shaped eyes
-    "🙀", # weary cat face
-    "😇", # smiling face with halo
-    "😈", # smiling face with horns
-    "😎", # smiling face with sunglasses
-    "😐", # neutral face
-    "😙", # kissing face with smiling eyes
-    "😛", # face with stuck-out tongue
-    "😧", # anguished face
-    "😬", # grimacing face
-    "😮", # face with open mouth
-    "😴", # sleeping face
-    "🙅", # face with no good gesture
-    "🙊", # speak-no-evil monkey
-    "🙋", # happy person raising one hand
-    "🙌", # person raising both hands in celebration
-    "🚀", # rocket
-    "🚬", # smoking symbol
-    "⛔", # no entry
-    "🌟", # glowing star
-    "🌵", # cactus
-    "🌷", # tulip
-    "🌿", # herb
-    "🍄", # mushroom
-    "🍆", # aubergine
-    "🍑", # peach
-    "🍓", # strawberry
-    "🍪", # cookie
-    "🍰", # shortcake
-    "🍴", # fork and knife
-    "🍷", # wine glass
-    "🍺", # beer mug
-    "🎁", # wrapped present
-    "🎓", # graduation cap
-    "🎲", # game die                           44
-    "🎹", # musical keyboard
-    "🏁", # chequered flag
-    "🏩", # love hotel
-    "🐍", # snake
-    "🐧", # penguin
-    "🐨", # koala
-    "🐬", # dolphin
-    "🐭", # mouse face
-    "🐮", # cow face
-    "🐰", # rabbit face
-    "🐱", # cat face
-    "🐲", # dragon face
-    "🐳", # spouting whale
-    "🐶", # dog face
-    "🐷", # pig face
-    "🐸", # frog face
-    "🐹", # hamster face
-    "🐻", # bear face
-    "🐼", # panda face
-    "👀", # eyes
-    "👊", # fisted hand sign
-    "👋", # waving hand sign
-    "👌", # ok hand sign
-    "👍", # thumbs up sign
-    "👎", # thumbs down sign
-    "👏", # clapping hands sign
-    "👑", # crown
-    "👓", # eyeglasses
-    "👔", # necktie
-    "👫", # man and woman holding hands
-    "👮", # police officer
-    "👯", # woman with bunny ears
-    "👶", # baby
-    "👷", # construction worker
-    "👸", # princess
-    "👻", # ghost
-    "👼", # baby angel
-    "👽", # extraterrestrial alien
-    "👾", # alien monster
-    "💀", # skull
-    "💁", # information desk person
-    "💂", # guardsman
-    "💃", # dancer
-    "💍", # ring
-    "💐", # bouquet
-    "💑", # couple with heart
-    "💔", # broken heart
-    "💘", # heart with arrow
-    "💣", # bomb                               93
-    "💦", # splashing sweat symbol
-    "💩", # pile of poo
-    "💪", # flexed biceps
-    "💯", # hundred points symbol
-    "💰", # money bag
-    "💾", # floppy disk
-    "📖", # open book
-    "📢", # public address loudspeaker
-    "📦", # package
-    "🔊", # speaker with three sound waves
-    "🔍", # left-pointing magnifying glass
-    "🔒", # lock
-    "🔔", # bell
-    "🔥", # fire
-    "🔨", # hammer
-    "🔪", # hocho
-    "🔫", # pistol
-    "🔮", # crystal ball
-    "🗻", # mount fuji
-    "🗽", # statue of liberty
-    "🚁", # helicopter
-    "🚊", # tram
-    "🚔", # oncoming police car
-    "🚜", # tractor                            117
-    "🚝", # monorail
-    "🚦", # vertical traffic light
-    "🚴", # bicyclist
-    "🛁", # bathtub
-    "🌝", # full moon with face
-    "🌲", # evergreen tree
-    "🍋", # lemon
-    "🍼", # baby bottle
-    "🏉", # rugby football
-    "👬", # two men holding hands
-    "👭", # two women holding hands
-    "🔉", # speaker with one sound wave
-    "🔬", # microscope
-    "🐉", #
-    "👹", #
-    "👺", #
-    "💉"  #                                    134
-]
+EMOJI_LIST = (
+    ("😁", "😁"), # grinning face with smiling eyes
+    ("😂", "😂"), # face with tears of joy
+    ("😃", "😃"), # smiling face with open mouth
+    ("😉", "😉"), # winking face
+    ("😍", "😍"), # smiling face with heart-shaped eyes
+    ("😏", "😏"), # smirking face
+    ("😘", "😘"), # face throwing a kiss
+    ("😡", "😡"), # pouting face
+    ("😱", "😱"), # face screaming in fear
+    ("😵", "😵"), # dizzy face
+    ("😻", "😻"), # smiling cat face with heart-shaped eyes
+    ("🙀", "🙀"), # weary cat face
+    ("😇", "😇"), # smiling face with halo
+    ("😈", "😈"), # smiling face with horns
+    ("😎", "😎"), # smiling face with sunglasses
+    ("😐", "😐"), # neutral face
+    ("😙", "😙"), # kissing face with smiling eyes
+    ("😛", "😛"), # face with stuck-out tongue
+    ("😧", "😧"), # anguished face
+    ("😬", "😬"), # grimacing face
+    ("😮", "😮"), # face with open mouth
+    ("😴", "😴"), # sleeping face
+    ("🙅", "🙅"), # face with no good gesture
+    ("🙊", "🙊"), # speak-no-evil monkey
+    ("🙋", "🙋"), # happy person raising one hand
+    ("🙌", "🙌"), # person raising both hands in celebration
+    ("🚀", "🚀"), # rocket
+    ("🚬", "🚬"), # smoking symbol
+    ("⛔", "⛔"), # no entry
+    ("🌟", "🌟"), # glowing star
+    ("🌵", "🌵"), # cactus
+    ("🌷", "🌷"), # tulip
+    ("🌿", "🌿"), # herb
+    ("🍄", "🍄"), # mushroom
+    ("🍆", "🍆"), # aubergine
+    ("🍑", "🍑"), # peach
+    ("🍓", "🍓"), # strawberry
+    ("🍪", "🍪"), # cookie
+    ("🍰", "🍰"), # shortcake
+    ("🍴", "🍴"), # fork and knife
+    ("🍷", "🍷"), # wine glass
+    ("🍺", "🍺"), # beer mug
+    ("🎁", "🎁"), # wrapped present
+    ("🎓", "🎓"), # graduation cap
+    ("🎲", "🎲"), # game die                           44
+    ("🎹", "🎹"), # musical keyboard
+    ("🏁", "🏁"), # chequered flag
+    ("🏩", "🏩"), # love hotel
+    ("🐍", "🐍"), # snake
+    ("🐧", "🐧"), # penguin
+    ("🐨", "🐨"), # koala
+    ("🐬", "🐬"), # dolphin
+    ("🐭", "🐭"), # mouse face
+    ("🐮", "🐮"), # cow face
+    ("🐰", "🐰"), # rabbit face
+    ("🐱", "🐱"), # cat face
+    ("🐲", "🐲"), # dragon face
+    ("🐳", "🐳"), # spouting whale
+    ("🐶", "🐶"), # dog face
+    ("🐷", "🐷"), # pig face
+    ("🐸", "🐸"), # frog face
+    ("🐹", "🐹"), # hamster face
+    ("🐻", "🐻"), # bear face
+    ("🐼", "🐼"), # panda face
+    ("👀", "👀"), # eyes
+    ("👊", "👊"), # fisted hand sign
+    ("👋", "👋"), # waving hand sign
+    ("👌", "👌"), # ok hand sign
+    ("👍", "👍"), # thumbs up sign
+    ("👎", "👎"), # thumbs down sign
+    ("👏", "👏"), # clapping hands sign
+    ("👑", "👑"), # crown
+    ("👓", "👓"), # eyeglasses
+    ("👔", "👔"), # necktie
+    ("👫", "👫"), # man and woman holding hands
+    ("👮", "👮"), # police officer
+    ("👯", "👯"), # woman with bunny ears
+    ("👶", "👶"), # baby
+    ("👷", "👷"), # construction worker
+    ("👸", "👸"), # princess
+    ("👻", "👻"), # ghost
+    ("👼", "👼"), # baby angel
+    ("👽", "👽"), # extraterrestrial alien
+    ("👾", "👾"), # alien monster
+    ("💀", "💀"), # skull
+    ("💁", "💁"), # information desk person
+    ("💂", "💂"), # guardsman
+    ("💃", "💃"), # dancer
+    ("💍", "💍"), # ring
+    ("💐", "💐"), # bouquet
+    ("💑", "💑"), # couple with heart
+    ("💔", "💔"), # broken heart
+    ("💘", "💘"), # heart with arrow
+    ("💣", "💣"), # bomb                               93
+    ("💦", "💦"), # splashing sweat symbol
+    ("💩", "💩"), # pile of poo
+    ("💪", "💪"), # flexed biceps
+    ("💯", "💯"), # hundred points symbol
+    ("💰", "💰"), # money bag
+    ("💾", "💾"), # floppy disk
+    ("📖", "📖"), # open book
+    ("📢", "📢"), # public address loudspeaker
+    ("📦", "📦"), # package
+    ("🔊", "🔊"), # speaker with three sound waves
+    ("🔍", "🔍"), # left-pointing magnifying glass
+    ("🔒", "🔒"), # lock
+    ("🔔", "🔔"), # bell
+    ("🔥", "🔥"), # fire
+    ("🔨", "🔨"), # hammer
+    ("🔪", "🔪"), # hocho
+    ("🔫", "🔫"), # pistol
+    ("🔮", "🔮"), # crystal ball
+    ("🗻", "🗻"), # mount fuji
+    ("🗽", "🗽"), # statue of liberty
+    ("🚁", "🚁"), # helicopter
+    ("🚊", "🚊"), # tram
+    ("🚔", "🚔"), # oncoming police car
+    ("🚜", "🚜"), # tractor                            117
+    ("🚝", "🚝"), # monorail
+    ("🚦", "🚦"), # vertical traffic light
+    ("🚴", "🚴"), # bicyclist
+    ("🛁", "🛁"), # bathtub
+    ("🌝", "🌝"), # full moon with face
+    ("🌲", "🌲"), # evergreen tree
+    ("🍋", "🍋"), # lemon
+    ("🍼", "🍼"), # baby bottle
+    ("🏉", "🏉"), # rugby football
+    ("👬", "👬"), # two men holding hands
+    ("👭", "👭"), # two women holding hands
+    ("🔉", "🔉"), # speaker with one sound wave
+    ("🔬", "🔬"), # microscope
+    ("🐉", "🐉"), #
+    ("👹", "👹"), #
+    ("👺", "👺"), #
+    ("💉", "💉")  #                                    134
+)
