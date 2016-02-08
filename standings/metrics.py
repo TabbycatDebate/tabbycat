15904f0902ff0875aeed0eb4ce08e711178c5fb9
Chuan-Zheng Lee <czlee@stanford.edu>
2016-02-08 15:33:20 -0800

    Make wins annotator a wins annotator (not points)

--- standings/metrics.py
+++ standings/metrics.py
@@ -92,6 +92,7 @@ class TeamScoreQuerySetMetricAnnotator(BaseMetricAnnotator):
     function = NotImplemented
     field = NotImplemented
     exclude_forfeits = False
+    where_value = None
 
     @classmethod
     def get_annotated_queryset(cls, queryset, field, function, round=None, column_name="metric"):
@@ -125,6 +126,10 @@ class TeamScoreQuerySetMetricAnnotator(BaseMetricAnnotator):
             TEAM_SCORE_ANNOTATION_QUERY += """
             AND "results_teamscore"."forfeit" = FALSE"""
 
+        if cls.where_value is not None:
+            TEAM_SCORE_ANNOTATION_QUERY += """
+            AND "{field:s}" = """ + str(cls.where_value)
+
         query = TEAM_SCORE_ANNOTATION_QUERY.format(field=field, function=function)
         logger.info("Running query: " + query)
 
@@ -147,14 +152,14 @@ class PointsMetricAnnotator(TeamScoreQuerySetMetricAnnotator):
 
 
 class WinsMetricAnnotator(TeamScoreQuerySetMetricAnnotator):
-    """Metric annotator for total number of points.
-    The difference between this and PointsMetricAnnotator is cosmetic only."""
+    """Metric annotator for total number of wins."""
     key = "wins"
     name = "wins"
     abbr = "Wins"
 
-    function = "SUM"
-    field = "points"
+    function = "COUNT"
+    field = "win"
+    where_value = "TRUE"
 
 
 class TotalSpeakerScoreMetricAnnotator(TeamScoreQuerySetMetricAnnotator):
@@ -206,6 +211,7 @@ class DrawStrengthMetricAnnotator(BaseMetricAnnotator):
     abbr = "DS"
 
     def annotate_teams(self, queryset, standings, round=None):
+        logger.info("Running points query for draw strength:")
         full_queryset = TeamScoreQuerySetMetricAnnotator.get_annotated_queryset(
                 queryset[0].tournament.team_set.all(), "points", "SUM", round, "points")
 
