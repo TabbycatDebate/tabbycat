7e7025f69dd54aa094cec470cba2732db330e3e2
Chuan-Zheng Lee <czlee@stanford.edu>
2016-02-20 12:59:04 -0800

    Change standings URL names to hyphenated app-first style

--- standings/urls_admin.py
+++ standings/urls_admin.py
@@ -3,13 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',          views.standings_index,                 name='standings_index'),
-    url(r'^team/$',     views.TeamStandingsView.as_view(),     name='team_standings'),
-    url(r'^division/$', views.DivisionStandingsView.as_view(), name='division_standings'),
+    url(r'^$',          views.standings_index,                 name='standings-index'),
+    url(r'^team/$',     views.TeamStandingsView.as_view(),     name='standings-team'),
+    url(r'^division/$', views.DivisionStandingsView.as_view(), name='standings-division'),
 
-    url(r'^speaker/$',  views.speaker_standings,               name='speaker_standings'),
-    url(r'^novices/$',  views.novice_standings,                name='novice_standings'),
-    url(r'^reply/$',    views.reply_standings,                 name='reply_standings'),
-    url(r'^motions/$',  views.motion_standings,                name='motion_standings'),
+    url(r'^speaker/$',  views.speaker_standings,               name='standings-speaker'),
+    url(r'^novices/$',  views.novice_standings,                name='standings-novice'),
+    url(r'^reply/$',    views.reply_standings,                 name='standings-reply'),
+    url(r'^motions/$',  views.motion_standings,                name='standings-motion'),
 
 ]
