33471290afd03896b02487ac4c134aa7a7c45c58
Philip Belesky <contact@philipbelesky.com>
2016-02-21 18:52:26 +1100

    Fix public team standings
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- standings/urls_public.py
+++ standings/urls_public.py
@@ -3,12 +3,22 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
-    url(r'^current/$',  views.public_team_standings,       name='standings-public-teams-current'),
-    url(r'^team/$',     views.PublicTeamTabView.as_view(), name='standings-public-tab-team'),
-    url(r'^speaker/$',  views.public_speaker_tab,          name='standings-public-tab-speaker'),
-    url(r'^novices/$',  views.public_novices_tab,          name='standings-public-tab-novices'),
-    url(r'^replies/$',  views.public_replies_tab,          name='standings-public-tab-replies'),
-    url(r'^motions/$',  views.public_motions_tab,          name='standings-public-tab-motions'),
-
+    url(r'^current_standings/$',
+        views.public_team_standings,
+        name='standings-public-teams-current'),
+    url(r'^team/$',
+        views.PublicTeamTabView.as_view(),
+        name='standings-public-tab-team'),
+    url(r'^speaker/$',
+        views.public_speaker_tab,
+        name='standings-public-tab-speaker'),
+    url(r'^novices/$',
+        views.public_novices_tab,
+        name='standings-public-tab-novices'),
+    url(r'^replies/$',
+        views.public_replies_tab,
+        name='standings-public-tab-replies'),
+    url(r'^motions/$',
+        views.public_motions_tab,
+        name='standings-public-tab-motions'),
 ]
