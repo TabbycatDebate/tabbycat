78818fc7e863c5eb87ef4d58aac5f8bcd88bdc0c
Philip Belesky <contact@philipbelesky.com>
2016-02-28 16:21:07 +1100

    Fixes to table displays
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- options/presets.py
+++ options/presets.py
@@ -155,7 +155,7 @@ class UADCPreferences(AustralsPreferences):
 class WADLPreferences(PreferencesPreset):
     """ Example high school league setup """
     def __init__(self):
-        self.name                                      = "WADL Options & "
+        self.name                                      = "WADL Options"
         self.show_in_list                              = True
         # Debate Rules= no replies; singular motions
         self.debate_rules__substantive_speakers        = 3
@@ -180,6 +180,9 @@ class WADLPreferences(PreferencesPreset):
         self.ui_options__show_speakers_in_draw         = False
         self.ui_options__public_motions_descending     = True
         self.ui_options__show_all_draws                = True
+        self.public_features__public_draw              = True
+        self.public_features__public_results           = True
+        self.public_features__public_motions           = True
         # League Options
         self.league_options__enable_flagged_motions    = True
         self.league_options__enable_adj_notes          = True
