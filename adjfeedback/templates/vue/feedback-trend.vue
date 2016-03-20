7be17c9290f28d72f2fe401824e3fdd4b04a13c7
Philip Belesky <contact@philipbelesky.com>
2016-03-20 18:13:59 +1100

    WIP on feedback graphs
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

---
+++ adjfeedback/templates/vue/feedback-trend.vue
@@ -0,0 +1,13 @@
+<!-- Adjudicator's Mini Feedback Graph -->
+<script type="text/x-template" id="feedback-trend">
+
+  hello
+
+</script>
+
+<script>
+  Vue.component('feedback-trend', {
+    template: '#feedback-trend',
+    props: ['data', 'ballot', 'motions'],
+  })
+</script>
