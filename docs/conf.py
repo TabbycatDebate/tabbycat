f6c9d04b8f41e2380d8cd6ac1f3cbb0429ea3125
Philip Belesky <contact@philipbelesky.com>
2016-04-28 23:36:24 +1000

    Update readthedocs URL as per their notice
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- docs/conf.py
+++ docs/conf.py
@@ -308,8 +308,8 @@ else:
     # Override default css to get a larger width for ReadTheDoc build
     html_context = {
         'css_files': [
-            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+            'https://media.readthedocs.io/css/sphinx_rtd_theme.css',
+            'https://media.readthedocs.io/css/readthedocs-doc-embed.css',
             '_static/theme_overrides.css',
         ],
     }
