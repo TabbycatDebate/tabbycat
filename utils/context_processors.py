6280916ac10d5e1dbd0f6dff70b6a1e9593a70ad
Philip Belesky <contact@philipbelesky.com>
2016-05-22 18:32:44 +1000

    Add some more info to footer; update some nav highlights
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -29,41 +29,45 @@ def debate_context(request):
 def get_menu_highlight(request):
     if "side_allocations" in request.path:
         return {'sides_nav': True}
-    elif "ballots" in request.path:
-        return {'ballots_nav': True}
-    elif "motions" in request.path:
-        return {'motions_nav': True}
-    elif "results" in request.path:
-        return {'ballots_nav': True}
-    elif "draw" in request.path:
-        return {'draw_nav': True}
-    elif "participants" in request.path:
-        return {'participants_nav': True}
-    elif "feedback" in request.path:
-        return {'feedback_nav': True}
     elif "availability" in request.path:
         return {'availability_nav': True}
-    elif "division_allocations" in request.path:
-        return {'divisions_nav': True}
-    elif "standings" in request.path:
-        return {'standings_nav': True}
-    elif "option" in request.path:
-        return {'options_nav': True}
-    elif "import" in request.path:
-        return {'import_nav': True}
+    elif "ballots" in request.path:
+        return {'ballots_nav': True}
     elif "break" in request.path:
         return {'break_nav': True}
+    elif "division_allocations" in request.path:
+        return {'divisions_nav': True}
+    elif "draw" in request.path and "admin" in request.path:
+        return {'draw_nav': True}
+    elif "feedback_progress" in request.path:
+        return {'feedback_progress_nav': True}
+    elif "feedback" in request.path:
+        return {'feedback_nav': True}
+    elif "import" in request.path:
+        return {'import_nav': True}
+    elif "motions" in request.path:
+        return {'motions_nav': True}
+    elif "option" in request.path:
+        return {'options_nav': True}
     elif "overview" in request.path:
         return {'overview_nav': True}
+    elif "participants" in request.path:
+        return {'participants_nav': True}
+    elif "results" in request.path and "admin" in request.path:
+        return {'ballots_nav': True}
+    elif "results" in request.path:
+        return {'results_nav': True}
+    elif "standings" in request.path:
+        return {'standings_nav': True}
     elif "tab" in request.path and "team" in request.path:
         return {'tab_team_nav': True}
     elif "tab" in request.path and "speaker" in request.path:
         return {'tab_speaker_nav': True}
+    elif "tab" in request.path and "pros" in request.path:
+        return {'tab_pros_nav': True}
     elif "tab" in request.path and "novices" in request.path:
         return {'tab_novices_nav': True}
     elif "tab" in request.path and "replies" in request.path:
         return {'tab_replies_nav': True}
-    elif "tab" in request.path and "motions" in request.path:
-        return {'tab_motions_nav': True}
     else:
         return {}  # Context processors must return a dict
