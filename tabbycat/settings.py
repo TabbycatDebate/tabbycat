98a2f3329acde98dfe9ff85465bff54d8d14a8bc
Philip Belesky <contact@philipbelesky.com>
2016-06-21 11:12:55 +1000

    Maybe get the gulp commands right

--- tabbycat/settings.py
+++ tabbycat/settings.py
@@ -149,7 +149,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # When running server side always use build not watch
 GULP_PRODUCTION_COMMAND = "npm run gulp build -- --production"
-GULP_DEVELOP_COMMAND = "npm run gulp build"
+GULP_DEVELOP_COMMAND = "npm run gulp build -- --development"
 
 # ==============================================================================
 # Logging
