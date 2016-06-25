9609ee71eb2a5c4cdbb87350aa4794b3f6b0b902
Philip Belesky <contact@philipbelesky.com>
2016-06-25 22:44:44 +1000

    Add break rounds/category size to the New Tournament wizard closes #325
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- tabbycat/tournaments/forms.py
+++ tabbycat/tournaments/forms.py
@@ -2,7 +2,9 @@ from django.forms.fields import IntegerField
 from django.forms import ModelForm
 
 from .models import Tournament
-from .utils import auto_make_rounds
+from .utils import auto_make_finals_rounds, auto_make_rounds
+
+from breakqual.models import BreakCategory
 
 
 class TournamentForm(ModelForm):
@@ -11,11 +13,43 @@ class TournamentForm(ModelForm):
         model = Tournament
         fields = ('name', 'short_name', 'slug')
 
-    num_prelim_rounds = IntegerField(min_value=1, label="Number of preliminary rounds")
+    num_prelim_rounds = IntegerField(
+        min_value=1,
+        label="Number of preliminary rounds")
+    num_final_rounds = IntegerField(
+        min_value=1,
+        required=False,
+        label="Number of finals rounds",
+        help_text="Leave blank if there are no finals rounds")
+
+    break_size = IntegerField(
+        min_value=2,
+        required=False,
+        label="Number of teams in the open break",
+        help_text="Leave blank if there are no finals rounds")
 
     def save(self):
         tournament = super(TournamentForm, self).save()
         auto_make_rounds(tournament, self.cleaned_data["num_prelim_rounds"])
+
+        if self.cleaned_data["break_size"]:
+            open_break = BreakCategory(
+                tournament=tournament,
+                name="Open",
+                slug="open",
+                seq=1,
+                break_size=self.cleaned_data["break_size"],
+                is_general=True,
+                priority=100
+            ).save()
+        else:
+            open_break = None
+
+        if self.cleaned_data["num_final_rounds"]:
+            auto_make_finals_rounds(tournament,
+                self.cleaned_data["num_prelim_rounds"],
+                self.cleaned_data["num_final_rounds"], open_break)
+
         tournament.current_round = tournament.round_set.first()
         tournament.save()
         return tournament
--- tabbycat/tournaments/utils.py
+++ tabbycat/tournaments/utils.py
@@ -16,3 +16,29 @@ def auto_make_rounds(tournament, num_rounds):
             feedback_weight=min((i-1)*0.1, 0.5),
             silent=(i == num_rounds),
         ).save()
+
+
+def auto_make_finals_rounds(tournament, num_rounds, num_finals, break_category):
+    """Makes the number of final rounds specified. This is intended as a
+    convenience function. For anything more complicated, a more advanced import
+    method should be used."""
+
+    finals_names = [
+        ('Grand Final', 'GF'), ('Semi Finals', 'SF'), ('Quarter Finals', 'QF'),
+        ('Octo Finals', 'OF'), ('Double-Octo Finals', 'DOF')]
+    finals_names.extend(
+        [('Unknown Finals', 'UF')] * (num_finals - len(finals_names)))
+
+    j = 0
+    for i in range(num_rounds+num_finals+1, num_rounds+1, -1):
+        Round(
+            tournament=tournament,
+            break_category=break_category,
+            seq=i,
+            name=finals_names[j][0],
+            abbreviation=finals_names[j][1],
+            draw_type=Round.DRAW_FIRSTBREAK if (i == num_rounds+1) else Round.DRAW_BREAK,
+            feedback_weight=min((i-1)*0.1, 0.5),
+            silent=True,
+        ).save()
+        j += 1
