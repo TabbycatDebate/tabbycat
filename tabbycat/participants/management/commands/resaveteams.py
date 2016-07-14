1262025f07a43c91a7f69b284fa3bb1672ac5d7f
Chuan-Zheng Lee <czlee@stanford.edu>
2016-07-14 23:27:13 +0800

    Elaborate help string in resaveteams

--- tabbycat/participants/management/commands/resaveteams.py
+++ tabbycat/participants/management/commands/resaveteams.py
@@ -3,7 +3,10 @@ from utils.management.base import TournamentCommand
 
 class Command(TournamentCommand):
 
-    help = "Resaves all teams to update their short and long name fields."
+    help = "Resaves all teams which updates their short and long name fields. " \
+           "This shouldn't generally be necessary, because the names are " \
+           "auto-populated whenever institutions and teams are saved, but it " \
+           "can be used when there was a mishap with team names."
 
     def handle_tournament(self, tournament, **options):
         for team in tournament.team_set.all():
