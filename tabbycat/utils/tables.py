96c5e4df3063853830714ee23bdf574be8a5089d
Chuan-Zheng Lee <czlee@stanford.edu>
2016-07-14 22:48:43 +0800

    Show spaces between venue groups and venue names

--- tabbycat/utils/tables.py
+++ tabbycat/utils/tables.py
@@ -461,7 +461,7 @@ class TabbycatTableBuilder(BaseTableBuilder):
         if self.tournament.pref('enable_venue_groups'):
             venue_data = [
                 debate.division.venue_group.short_name if debate.division
-                else (debate.venue.group.short_name + debate.venue.name) if debate.venue
+                else (debate.venue.group.short_name + ' ' + debate.venue.name) if debate.venue
                 else ''
                 for debate in debates
             ]
