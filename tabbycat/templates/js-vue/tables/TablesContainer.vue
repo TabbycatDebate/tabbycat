786518ffbfb5d47cb1f6dd372711d5d48aa1246b
Philip Belesky <contact@philipbelesky.com>
2016-07-17 10:34:57 +0800

    Fix table searching, change its layout

--- tabbycat/templates/js-vue/tables/TablesContainer.vue
+++ tabbycat/templates/js-vue/tables/TablesContainer.vue
@@ -1,12 +1,19 @@
 <template>
 
   <div class="row">
+    <div class="col-md-12 half-vertical-spacing">
+      <div class="input-group">
+        <input id="table-search" type="search" class="form-control table-search" placeholder="Find in Table">
+        <span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>
+      </div>
+    </div>
+  </div>
 
+  <div class="row">
     <div v-for="(tableIndex, tableData) in tablesData"
          v-bind:class="{'col-md-6': tablesData.length === 2,
                         'col-md-12': tablesData.length === 1}">
-
-      <div class="panel panel-default" id="tableContainer{{ table_index }}">
+      <div class="panel panel-default table-continer" id="tableContainer{{ table_index }}">
         <div class="panel-heading" v-if="tableData['title']">
           <h4 class="panel-title">{{ tableData['title'] }}</h4>
         </div>
@@ -15,15 +22,13 @@
             :table-headers="tableData['head']"
             :table-content="tableData['data']"
             :table-class="tableData['class']"
-            :filter-key=""
+            :filter-key="filterKey"
             :default-sort-key="tableData['sort_key']"
             :default-sort-order="tableData['sort_order']">
           </smart-table>
         </div>
       </div>
-
     </div>
-
   </div>
 
 </template>
@@ -36,7 +41,14 @@ export default {
     SmartTable
   },
   props: {
-    tablesData: Array // Passed down from main.js
+    tablesData: Array, // Passed down from main.js
+    filterKey: { default: '' }
+  },
+  ready: function() {
+    var self = this;
+    $('#table-search').keyup(function() {
+      self.filterKey = $(this).val();
+    });
   }
 }
 
