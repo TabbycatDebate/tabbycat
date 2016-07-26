9a064c32d1e0dad89deb9adb95cb80c3aef1375e
Chuan-Zheng Lee <czlee@stanford.edu>
2016-07-26 08:05:05 +0800

    Change draw public view names to new convention

--- tabbycat/draw/tests/test_views.py
+++ tabbycat/draw/tests/test_views.py
@@ -4,7 +4,7 @@ from tournaments.models import Round
 
 
 class PublicDrawForRoundViewTest(ConditionalTableViewTest, TestCase):
-    view_name = 'public_draw_by_round'
+    view_name = 'draw-public-for-round'
     view_toggle = 'public_features__public_draw'
     round_seq = 2
 
