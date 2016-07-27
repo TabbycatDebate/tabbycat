9ca62c4ebd60b6d900c62a7b774f16250325f6e4
Chuan-Zheng Lee <czlee@stanford.edu>
2016-07-27 10:27:33 +0800

    Revert misguided import removals

--- tabbycat/standings/views.py
+++ tabbycat/standings/views.py
@@ -3,8 +3,10 @@ import json
 from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.contrib import messages
+from django.db.models import Count
 
 import motions.statistics as motion_statistics
+from motions.models import Motion
 from participants.models import Speaker, Team
 from results.models import SpeakerScore, TeamScore
 from tournaments.mixins import PublicTournamentPageMixin, RoundMixin, TournamentMixin
