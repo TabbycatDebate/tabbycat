13d3968ef9e379a45831899f3991af1637a6bb1a
Chuan-Zheng Lee <czlee@stanford.edu>
2016-08-18 20:41:08 -0700

    Bump version to 1.0.2-dev

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ author = 'Philip Belesky, Chuan-Zheng Lee'
 # The short X.Y version.
 version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.0.1'
+release = '1.0.2-dev'
 
 rst_epilog = """
 .. |vrelease| replace:: v{release}
