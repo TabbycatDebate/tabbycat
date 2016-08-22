95dd72f0d3516d0a34655ca70321b1b606c9ffab
Chuan-Zheng Lee <czlee@stanford.edu>
2016-08-22 00:41:14 -0700

    flake8

--- tabbycat/tournaments/mixins.py
+++ tabbycat/tournaments/mixins.py
@@ -4,8 +4,8 @@ from django.core.cache import cache
 from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import NoReverseMatch
 from django.contrib import messages
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
-from django.views.generic.base import ContextMixin
 from django.views.generic.detail import SingleObjectMixin
 
 from utils.misc import redirect_tournament, reverse_round, reverse_tournament
