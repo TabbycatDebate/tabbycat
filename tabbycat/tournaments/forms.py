23acc0a01c702c2fe053b88959eaf08f81afe557
Chuan-Zheng Lee <czlee@stanford.edu>
2016-08-27 14:30:07 -0700

    Fix TournamentForm error where it crashes if break_size is left blank

--- tabbycat/tournaments/forms.py
+++ tabbycat/tournaments/forms.py
@@ -29,25 +29,23 @@ class TournamentForm(ModelForm):
         tournament = super(TournamentForm, self).save()
         auto_make_rounds(tournament, self.cleaned_data["num_prelim_rounds"])
 
-        if self.cleaned_data["break_size"] > 0:
-            num_break_rounds = math.ceil(math.log2(
-                self.cleaned_data["break_size"]))
-
+        break_size = self.cleaned_data["break_size"]
+        if break_size:
             open_break = BreakCategory(
                 tournament=tournament,
                 name="Open",
                 slug="open",
                 seq=1,
-                break_size=self.cleaned_data["break_size"],
+                break_size=break_size,
                 is_general=True,
                 priority=100
             )
+            open_break.full_clean()
             open_break.save()
-        else:
-            open_break = None
 
-        auto_make_break_rounds(tournament,
-            self.cleaned_data["num_prelim_rounds"], num_break_rounds, open_break)
+            num_break_rounds = math.ceil(math.log2(break_size))
+            auto_make_break_rounds(tournament, self.cleaned_data["num_prelim_rounds"],
+                    num_break_rounds, open_break)
 
         tournament.current_round = tournament.round_set.first()
         tournament.save()
