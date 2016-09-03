e38f0387642ce14a9d560bf462bcc3cabbb23321
Chuan-Zheng Lee <czlee@stanford.edu>
2016-09-03 15:33:42 -0700

    Fix public results view test

--- tabbycat/results/tests/test_views.py
+++ tabbycat/results/tests/test_views.py
@@ -6,7 +6,7 @@ from tournaments.models import Round
 class PublicResultsForRoundViewTestCase(ConditionalTableViewTest, TestCase):
 
     view_toggle = 'public_features__public_results'
-    view_name = 'public_results'
+    view_name = 'results-public-round'
     round_seq = 3
 
     def table_data(self):
