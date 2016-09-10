efe5f4dbe51a891fa9ac395a9642b4f3f9973cea
Chuan-Zheng Lee <czlee@stanford.edu>
2016-09-09 21:37:39 -0700

    flake8

--- tabbycat/tournaments/signals.py
+++ tabbycat/tournaments/signals.py
@@ -1,5 +1,5 @@
 from django.core.cache import cache
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from tournaments.models import Round, Tournament
