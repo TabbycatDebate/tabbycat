8159f74ebfa335bdf971d39e861243d1b89a5e97
Chuan-Zheng Lee <czlee@stanford.edu>
2016-09-12 20:18:22 -0700

    Add to select_related of DebateTeamAdmin

--- tabbycat/draw/admin.py
+++ tabbycat/draw/admin.py
@@ -18,9 +18,8 @@ class DebateTeamAdmin(TabbycatModelAdminFieldsMixin, admin.ModelAdmin):
     raw_id_fields = ('debate', 'team', )
 
     def get_queryset(self, request):
-        return super(DebateTeamAdmin,
-                     self).get_queryset(request).select_related(
-                         'debate__round', 'debate__round__tournament')
+        return super(DebateTeamAdmin, self).get_queryset(request).select_related(
+                'debate__round', 'debate__round__tournament', 'team__tournament')
 
 
 admin.site.register(DebateTeam, DebateTeamAdmin)
