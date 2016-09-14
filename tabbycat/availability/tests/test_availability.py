1ab6521d79f3bd1f90f18eab1cebebe022eaf325
Chuan-Zheng Lee <czlee@stanford.edu>
2016-09-13 20:43:22 -0700

    Add better tests for availability

--- tabbycat/availability/tests/test_availability.py
+++ tabbycat/availability/tests/test_availability.py
@@ -2,18 +2,40 @@ from participants.models import Adjudicator
 from tournaments.models import Round
 from utils.tests import BaseDebateTestCase
 
-from ..utils import set_availability
+from ..utils import activate_all, set_availability
 
 
-class TestAdjudicatorDisable(BaseDebateTestCase):
+class TestAvailability(BaseDebateTestCase):
+
     def setUp(self):
-        super(TestAdjudicatorDisable, self).setUp()
+        super().setUp()
         self.round = Round(tournament=self.t, seq=1)
         self.round.save()
-        set_availability(Adjudicator.objects.exclude(name="Adjudicator00"), self.round)
 
-    def test_objects(self):
+    def tearDown(self):
+        super().tearDown()
+        self.round.delete()
+
+    def test_all_active(self):
+        set_availability(Adjudicator.objects.all(), self.round)
         self.assertEqual(8, Adjudicator.objects.count())
+        self.assertEqual(8, self.round.active_adjudicators.count())
 
-    def test_active(self):
+    def test_one_disabled(self):
+        set_availability(Adjudicator.objects.exclude(name="Adjudicator00"), self.round)
+        self.assertEqual(8, Adjudicator.objects.count())
         self.assertEqual(7, self.round.active_adjudicators.count())
+
+    def test_activate_all(self):
+        self.t.preferences['league_options__share_adjs'] = False
+        activate_all(self.round)
+        self.assertEqual(8, self.round.active_adjudicators.count())
+        self.assertEqual(12, self.round.active_teams.count())
+        self.assertEqual(8, self.round.active_venues.count())
+
+    def test_activate_relevant(self):
+        self.t.preferences['league_options__share_adjs'] = True
+        activate_all(self.round)
+        self.assertEqual(8, self.round.active_adjudicators.count())
+        self.assertEqual(12, self.round.active_teams.count())
+        self.assertEqual(16, self.round.active_venues.count())
