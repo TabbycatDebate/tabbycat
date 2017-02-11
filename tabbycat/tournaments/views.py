f1c3312654625815faf9ebfd6b69be31a476b0c4
Philip Belesky <contact@philipbelesky.com>
2017-02-11 17:11:01 +1100

    Add alerts if reimporting demo data

--- tabbycat/tournaments/views.py
+++ tabbycat/tournaments/views.py
@@ -176,27 +176,33 @@ class CreateTournamentView(SuperuserRequiredMixin, CreateView):
     form_class = TournamentForm
     template_name = "create_tournament.html"
 
+    def get_context_data(self, **kwargs):
+        kwargs["preexisting_small_demo"] = Tournament.objects.filter(slug="demo_simple").exists()
+        kwargs["preexisting_large_demo"] = Tournament.objects.filter(slug="demo").exists()
+        return super().get_context_data(**kwargs)
+
 
 class LoadDemoView(SuperuserRequiredMixin, PostOnlyRedirectView):
 
     def post(self, request, *args, **kwargs):
         source = request.POST.get("source", "")
-        if Tournament.objects.filter(slug=source).exists():
-            messages.warning(self.request, "This kind of demo tournament "
-                "already exists; you should delete it (and its institutions) "
-                "in the Edit Database Area before creating another demo.")
+
+        existing_tournament = Tournament.objects.filter(slug=source)
+        if existing_tournament.exists():
+            existing_tournament.delete()
+
+        try:
+            management.call_command(importtournament.Command(), source, relaxed=True)
+        except TournamentDataImporterError as e:
+            messages.error(self.request, mark_safe("<p>There were one or more errors creating the demo tournament. "
+                "Before retrying, please delete the existing demo tournament <strong>and</strong> "
+                "the institutions in the Edit Database Area.</p><p><i>Technical information: The errors are as follows:"
+                "<ul>" + "".join("<li>{}</li>".format(message) for message in e.itermessages()) + "</ul></i></p>"))
+            logger.critical("Error importing demo tournament: " + str(e))
         else:
-            try:
-                management.call_command(importtournament.Command(), source)
-            except TournamentDataImporterError as e:
-                messages.error(self.request, mark_safe("<p>There were one or more errors creating the demo tournament. "
-                    "Before retrying, please delete the existing demo tournament <strong>and</strong> "
-                    "the institutions in the Edit Database Area.</p><p><i>Technical information: The errors are as follows:"
-                    "<ul>" + "".join("<li>{}</li>".format(message) for message in e.itermessages()) + "</ul></i></p>"))
-                logger.critical("Error importing demo tournament: " + str(e))
-            else:
-                messages.success(self.request, "Created new demo tournament. You "
-                    "can access it below.")
+            messages.success(self.request, "Created new demo tournament. You "
+                "can access it below.")
+
         return redirect('tabbycat-index')
 
 
