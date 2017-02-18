f8a7566c2dbcda8e1f08e5591d1a5128ece60ed5
Chuan-Zheng Lee <czlee@stanford.edu>
2017-02-18 12:08:52 -0800

    Require HTTPS (work in progress, may break non-Heroku) #132

--- tabbycat/settings.py
+++ tabbycat/settings.py
@@ -242,6 +242,12 @@ except:
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Require HTTPS
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
