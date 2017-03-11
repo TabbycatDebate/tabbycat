45d417b6bad54696c300780786956d5a5f8de0a1
Philip Belesky <contact@philipbelesky.com>
2017-03-11 17:46:10 +1100

    Slight tweak to feedback form headings

--- tabbycat/templates/js-vue/printables/BallotHeader.vue
+++ tabbycat/templates/js-vue/printables/BallotHeader.vue
@@ -5,14 +5,14 @@
     <div class="db-align-vertical-end db-flex-item-3">
       <h2>
         {{ data.tournamentName }} {{ data.kind }} from {{ ballot.author }}
-        <span v-if="ballot.authorPosition === 'o'">(Solo Chair)</span>
-        <span v-if="ballot.authorPosition === 'c'">(Chair of Panel)</span>
-        <span v-if="ballot.authorPosition === 'p'">(Panellist)</span>
-        <span v-if="ballot.authorPosition === 't'">(Trainee)</span>
+        <span class="small" v-if="ballot.authorPosition === 'o'">(Solo Chair)</span>
+        <span class="small" v-if="ballot.authorPosition === 'c'">(Panel Chair)</span>
+        <span class="small" v-if="ballot.authorPosition === 'p'">(Panellist)</span>
+        <span class="small" v-if="ballot.authorPosition === 't'">(Trainee)</span>
         <span v-if="ballot.target">on {{ ballot.target }}
-          <span v-if="ballot.targetPosition === 'c' || ballot.targetPosition === 'o'">(Chair)</span>
-          <span v-if="ballot.targetPosition === 'p'">(Panellist)</span>
-          <span v-if="ballot.targetPosition === 't'">(Trainee)</span>
+          <span class="small" v-if="ballot.targetPosition === 'c' || ballot.targetPosition === 'o'">(Chair)</span>
+          <span class="small" v-if="ballot.targetPosition === 'p'">(Panellist)</span>
+          <span class="small" v-if="ballot.targetPosition === 't'">(Trainee)</span>
         </span>
       </h2>
     </div>
