519a8c813c1ed7cd91b13856574f5b81e6f641d5
Chuan-Zheng Lee <czlee@stanford.edu>
2017-04-19 15:33:34 -0700

    Management command minor touches

--- tabbycat/adjallocation/management/commands/readdinstitutionalconflicts.py
+++ tabbycat/adjallocation/management/commands/readdinstitutionalconflicts.py
@@ -9,10 +9,19 @@ class Command(TournamentCommand):
 
     def handle_tournament(self, tournament, **options):
 
+        existing = 0
+        missing = 0
+
         for adj in tournament.adjudicator_set.all():
             aic, created = AdjudicatorInstitutionConflict.objects.get_or_create(
                 adjudicator=adj, institution=adj.institution
             )
             if created:
+                missing += 1
                 self.stdout.write("Added self-institutional conflict for {adj.name} of "
                         "{adj.institution.name}".format(adj=adj))
+            else:
+                existing += 1
+
+        self.stdout.write("Done, created {missing} previously-missing self-institutional conflicts.".format(missing=missing))
+        self.stdout.write("{existing} adjudicators already had self-institutional conflicts defined.".format(existing=existing))
