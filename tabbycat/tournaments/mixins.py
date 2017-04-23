51dd8c3000b2a51e20b8562215203927c096bcde
Chuan-Zheng Lee <czlee@stanford.edu>
2017-04-22 17:02:54 -0700

    flake8

--- tabbycat/tournaments/mixins.py
+++ tabbycat/tournaments/mixins.py
@@ -83,8 +83,7 @@ class TournamentMixin(TabbycatPageTitlesMixin):
                 logger.critical("Current round wasn't set, redirecting to site index, was looking for %s" % full_path)
                 messages.error(request, _("There's a problem with the data for the tournament "
                     "%(tournament_name)s. Please contact a tab director and ask them to set its "
-                    "current round.") % {'tournament_name': tournament.name}
-                )
+                    "current round.") % {'tournament_name': tournament.name})
                 home_url = reverse('tabbycat-index')
                 redirect_url = add_query_parameter(home_url, 'redirect', 'false')
                 return HttpResponseRedirect(redirect_url)
