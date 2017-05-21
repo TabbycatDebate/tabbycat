d5a3a00bb9f81f3178b0c42625506d731db8d274
Chuan-Zheng Lee <czlee@stanford.edu>
2017-05-21 16:25:16 -0700

    Refactor importer for #461

--- tabbycat/importer/base.py
+++ tabbycat/importer/base.py
@@ -118,8 +118,7 @@ class BaseTournamentDataImporter(object):
         interpreter = make_interpreter(
             institution=lambda x: participants.models.Institution.objects.get(name=x)
         )
-        counts, errors = self._import(f, participants.models.Speaker, interpreter)
-        return counts, errors
+        self._import(f, participants.models.Speaker, interpreter)
 
     See the documentation for _import for more details.
     """
@@ -132,9 +131,11 @@ class BaseTournamentDataImporter(object):
             self.logger.setLevel(kwargs['loglevel'])
         self.expect_unique = kwargs.get('expect_unique', True)
 
-    def _import(self, csvfile, model, interpreter=make_interpreter(),
-                counts=None, errors=None, expect_unique=None,
-                generated_fields={}):
+    def reset_counts(self):
+        self.counts = Counter()
+        self.errors = TournamentDataImporterError()
+
+    def _import(self, csvfile, model, interpreter=make_interpreter(), expect_unique=None):
         """Parses the object given in f, using the callable line_parser to parse
         each line, and passing the arguments to the given model's constructor.
         `csvfile` can be any object that is supported by csv.reader(), which
@@ -152,41 +153,28 @@ class BaseTournamentDataImporter(object):
         each dict yielded. If omitted, the dict returned by `csv.DictReader`
         will be used directly.
 
-        Returns a tuple of two objects. The first is a Counter object (from
-        Python's collections module) in which the keys are models (e.g. Round,
-        Team) and the values are the number that were created. The second is a
-        (possibly empty) TournamentDataImporterError object. If self.strict is
-        True, then the returned TournamentDataImporterError will always be empty
-        (since otherwise it would have raised it as an exception); otherwise, it
-        will contain the errors raised during the import attempt.
+        Returns a list of instances created in this import.
 
-        If `counts` and/or `errors` are provided, this function adds the counts
-        and errors from this _import call and returns them instead. This
-        modifies the original counts_in and errors_in. This allows easy daisy-
-        chaining of successive _import calls. If provided, 'counts_in' should
-        behave like a Counter object and 'errors_in' should behave like a
-        TournamentDataImporterError object.
+        Callers may also access two attributes, which are updated every time
+        this function is called. The first, `self.counts` is a Counter object
+        (from Python's collections module) in which the keys are models (e.g.
+        Round, Team) and the values are the number that were created. The
+        second, `self.errors` is a (possibly empty) TournamentDataImporterError
+        object. If `self.strict` is True, then `self.errors` will always be
+        empty (since otherwise it would have raised it as an exception);
+        otherwise, it will contain the errors raised during the import attempt.
 
         If `expect_unique` is True, this function checks that there are no
         duplicate objects before saving any of the objects it creates. If
         `expect_unique` is False, it will just skip objects that would be
         duplicates and log a DUPLICATE_INFO message to say so.
-
-        If `generated_fields` is given, it must be a dict, with keys being field
-        names and values being callables. The uniqueness checks will not take
-        into account any of the generated fields. This should be used for fields
-        that are generated with each object, not given in the CSV files.
         """
         if hasattr(csvfile, 'seek') and callable(csvfile.seek):
             csvfile.seek(0)
         reader = csv.DictReader(csvfile)
         kwargs_seen = list()
-        insts = list()
-        if counts is None:
-            counts = Counter()
-        if errors is None:
-            errors = TournamentDataImporterError()
-        _errors = TournamentDataImporterError() # keep this run's errors separate
+        instances = list()
+        errors = TournamentDataImporterError()
         if expect_unique is None:
             expect_unique = self.expect_unique
         skipped_because_existing = 0
@@ -205,7 +193,7 @@ class BaseTournamentDataImporter(object):
             except (ObjectDoesNotExist, MultipleObjectsReturned, ValueError,
                     TypeError, IndexError) as e:
                 message = "Couldn't parse line: " + str(e)
-                _errors.add(lineno, model, message)
+                errors.add(lineno, model, message)
                 continue
 
             if kwargs_list is None:
@@ -221,15 +209,11 @@ class BaseTournamentDataImporter(object):
                 if kwargs_expect_unique in kwargs_seen:
                     if expect_unique:
                         message = "Duplicate " + description
-                        _errors.add(lineno, model, message)
+                        errors.add(lineno, model, message)
                     else:
                         self.logger.log(DUPLICATE_INFO, "Skipping duplicate " + description)
                     continue
                 kwargs_seen.append(kwargs_expect_unique)
-
-                # Fill in the generated fields
-                for key, generator_fn in generated_fields.items():
-                    kwargs[key] = generator_fn()
 
                 # Retrieve the instance or create it if it doesn't exist
                 try:
@@ -238,7 +222,7 @@ class BaseTournamentDataImporter(object):
                     inst = model(**kwargs)
                 except MultipleObjectsReturned as e:
                     if expect_unique:
-                        _errors.add(lineno, model, str(e))
+                        errors.add(lineno, model, str(e))
                     continue
                 except FieldError as e:
                     match = re.match("Cannot resolve keyword '(\w+)' into field.", str(e))
@@ -254,16 +238,16 @@ class BaseTournamentDataImporter(object):
                     else:
                         raise
                 except ValueError as e:
-                    _errors.add(lineno, model, str(e))
+                    errors.add(lineno, model, str(e))
                     continue
                 except ValidationError as e:
-                    _errors.update_with_validation_error(lineno, model, e)
+                    errors.update_with_validation_error(lineno, model, e)
                     continue
                 else:
                     skipped_because_existing += 1
                     if expect_unique:
                         message = description + " already exists"
-                        _errors.add(lineno, model, message)
+                        errors.add(lineno, model, message)
                     else:
                         self.logger.log(DUPLICATE_INFO, "Skipping %s, already exists", description)
                     continue
@@ -271,29 +255,30 @@ class BaseTournamentDataImporter(object):
                 try:
                     inst.full_clean()
                 except ValidationError as e:
-                    _errors.update_with_validation_error(lineno, model, e)
+                    errors.update_with_validation_error(lineno, model, e)
                     continue
 
                 self.logger.debug("Listing to create: " + description)
-                insts.append(inst)
+                instances.append(inst)
 
-        if _errors:
+        if errors:
             if self.strict:
-                for message in _errors.itermessages():
+                for message in errors.itermessages():
                     self.logger.error(message)
-                raise _errors
+                raise errors
             else:
-                for message in _errors.itermessages():
+                for message in errors.itermessages():
                     self.logger.warning(message)
-                errors.update(_errors)
+                self.errors.update(errors)
 
-        for inst in insts:
+        for inst in instances:
             self.logger.debug("Made %s: %r", model._meta.verbose_name, inst)
             inst.save()
 
-        self.logger.info("Imported %d %s", len(insts), model._meta.verbose_name_plural)
+        self.logger.info("Imported %d %s", len(instances), model._meta.verbose_name_plural)
         if skipped_because_existing:
             self.logger.info("(skipped %d %s)", skipped_because_existing, model._meta.verbose_name_plural)
 
-        counts.update({model: len(insts)})
-        return counts, errors
+        self.counts.update({model: len(instances)})
+
+        return instances
