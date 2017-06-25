e42202e047cbaed6e07ce9b188aacc604ceab59e
Philip Belesky <contact@philipbelesky.com>
2017-06-25 18:11:26 +1000

    Fix error where only first conflicts were recognised

--- tabbycat/adjallocation/utils.py
+++ tabbycat/adjallocation/utils.py
@@ -87,7 +87,8 @@ def populate_clashes(conflicts, conflict, type, for_type):
     # Make the base dictionary structure for each adj if it doesn't exist already
     if adj_or_team_id not in conflicts[for_type]:
         conflicts[for_type][adj_or_team_id] = {'team': [], 'institution': [], 'adjudicator': []}
-        conflicts[for_type][adj_or_team_id][type].append(conflictee_id)
+
+    conflicts[for_type][adj_or_team_id][type].append(conflictee_id)
     return conflicts
 
 
