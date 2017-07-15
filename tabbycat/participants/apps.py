f5d5d6a7923aea517dc2b56132d7f0240d92824e
Chuan-Zheng Lee <czlee@stanford.edu>
2017-07-15 14:48:28 -0700

    i18n

--- tabbycat/participants/apps.py
+++ tabbycat/participants/apps.py
@@ -1,8 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class ParticipantsConfig(AppConfig):
     name = 'participants'
+    verbose_name = _("Participants")
 
     def ready(self):
         from . import signals  # noqa: F401
