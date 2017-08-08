e4168abaafccb79687ab823b54829720f1399a69
Chuan-Zheng Lee <czlee@stanford.edu>
2017-08-08 00:24:52 -0700

    Delete obsolete standings views and tab preferences

--- tabbycat/options/dynamic_preferences_registry.py
+++ tabbycat/options/dynamic_preferences_registry.py
@@ -495,42 +495,6 @@ class SpeakerCategoryTabsReleased(BooleanPreference):
 
 
 @tournament_preferences_registry.register
-class ProsTabReleased(BooleanPreference):
-    help_text = "Enables public display of a pro-speakers only tab. Intended for use after the tournament."
-    verbose_name = "Release pros tab to public"
-    section = tab_release
-    name = "pros_tab_released"
-    default = False
-
-
-@tournament_preferences_registry.register
-class ProsTabReleaseLimit(IntegerPreference):
-    help_text = "Only show scores for the top X pro speakers in the public tab (set to 0 to show all pro speakers)."
-    verbose_name = "Top pros cutoff"
-    section = tab_release
-    name = "pros_tab_limit"
-    default = 0
-
-
-@tournament_preferences_registry.register
-class NovicesTabReleased(BooleanPreference):
-    help_text = "Enables public display of a novice-speakers only tab. Intended for use after the tournament."
-    verbose_name = "Release novices tab to public"
-    section = tab_release
-    name = "novices_tab_released"
-    default = False
-
-
-@tournament_preferences_registry.register
-class NovicesTabReleaseLimit(IntegerPreference):
-    help_text = "Only show scores for the top X novices in the public tab (set to 0 to show all novices)."
-    verbose_name = "Top novices cutoff"
-    section = tab_release
-    name = "novices_tab_limit"
-    default = 0
-
-
-@tournament_preferences_registry.register
 class RepliesTabReleased(BooleanPreference):
     help_text = "Enables public display of the replies tab. Intended for use after the tournament."
     verbose_name = "Release replies tab to public"
@@ -545,42 +509,6 @@ class RepliesTabReleaseLimit(IntegerPreference):
     verbose_name = "Top replies cutoff"
     section = tab_release
     name = "replies_tab_limit"
-    default = 0
-
-
-@tournament_preferences_registry.register
-class ESLTabReleased(BooleanPreference):
-    help_text = "Enables public display of the ESL speakers tab. Intended for use after the tournament."
-    verbose_name = "Release ESL speakers tab to public"
-    section = tab_release
-    name = "esl_tab_released"
-    default = False
-
-
-@tournament_preferences_registry.register
-class ESLTabReleaseLimit(IntegerPreference):
-    help_text = "Only show scores for the top X ESL speakers in the public tab (set to 0 to show all speakers)."
-    verbose_name = "Top ESL speakers cutoff"
-    section = tab_release
-    name = "esl_tab_limit"
-    default = 0
-
-
-@tournament_preferences_registry.register
-class EFLTabReleased(BooleanPreference):
-    help_text = "Enables public display of the EFL speakers tab. Intended for use after the tournament."
-    verbose_name = "Release EFL speakers tab to public"
-    section = tab_release
-    name = "efl_tab_released"
-    default = False
-
-
-@tournament_preferences_registry.register
-class EFLTabReleaseLimit(IntegerPreference):
-    help_text = "Only show scores for the top X EFL speakers in the public tab (set to 0 to show all speakers)."
-    verbose_name = "Top EFL speakers cutoff"
-    section = tab_release
-    name = "efl_tab_limit"
     default = 0
 
 
