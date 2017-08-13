b9e350834bd35366747382a32a8c39185663feba
Chuan-Zheng Lee <czlee@stanford.edu>
2017-08-12 22:18:14 -0700

    Fix adjudicator (and team) record pages

---
+++ tabbycat/participants/templatetags/team_link.py
@@ -0,0 +1,24 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+from utils.misc import reverse_tournament
+
+register = template.Library()
+
+@register.simple_tag
+def team_record_link(team, admin):
+    """Team record links are used often, so this template tag just reduces
+    clutter in templates, in particular in translated strings."""
+
+    if not team:
+        return ""
+
+    tournament = team.tournament
+    name = team.short_name
+
+    if admin:
+        url = reverse_tournament('participants-team-record', tournament, kwargs={'pk': team.pk})
+    else:
+        url = reverse_tournament('participants-public-team-record', tournament, kwargs={'pk': team.pk})
+
+    return mark_safe("""<a href="%(url)s" class="list-group-item-text">%(name)s</a>""" % {'url': url, 'name': name})
