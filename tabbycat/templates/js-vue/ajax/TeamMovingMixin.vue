f3de90bf011714ad41a8e31e051a578fd6ca59c3
Philip Belesky <contact@philipbelesky.com>
2017-08-23 22:40:39 +1000

    Clarify edit matchups editing procedure

--- tabbycat/templates/js-vue/ajax/TeamMovingMixin.vue
+++ tabbycat/templates/js-vue/ajax/TeamMovingMixin.vue
@@ -12,25 +12,13 @@ export default {
     },
     saveMoveForType(teamId, fromDebate, toDebate, toPosition) {
       var team = this.allTeamsById[teamId]
-      // Data Logic
-      if (toDebate === 'unused') {
-        var fromPosition = _.findKey(fromDebate.teams, team);
-        delete fromDebate.teams[fromPosition]
-        this.unallocatedItems.push(team) // Need to push; not append
-        // Update front end otherwise teams wont appear removed
-        this.$set(this.debatesById[fromDebate.id], 'teams', fromDebate.teams)
-      }
-      if (fromDebate === 'unused') {
-        if (toDebate.teams[toPosition]) { // If replacing a team
-          this.unallocatedItems.push(toDebate.teams[toPosition])
-        }
-        toDebate.teams[toPosition] = team
-        this.unallocatedItems.splice(this.unallocatedItems.indexOf(team), 1)
-      }
+
       if (toDebate !== 'unused' && fromDebate !== 'unused') {
+
+        // Moving from one debate to another
         var fromPosition = _.findKey(fromDebate.teams, team);
         if (toDebate.teams[toPosition]) {
-          // If replacing a team
+          // Replacing a team
           fromDebate.teams[fromPosition] = toDebate.teams[toPosition]
         } else {
           // If not replacing a team
@@ -38,7 +26,32 @@ export default {
           // Update front end otherwise teams wont appear removed
           this.$set(this.debatesById[fromDebate.id], 'teams', fromDebate.teams)
         }
+        // Set the move and update the front end
         toDebate.teams[toPosition] = team
+        this.$set(this.debatesById[toDebate.id], 'teams', toDebate.teams)
+
+      } else if (toDebate === 'unused') {
+
+        // Moving to the unused area
+        var fromPosition = _.findKey(fromDebate.teams, team);
+        delete fromDebate.teams[fromPosition]
+        this.unallocatedItems.push(team) // Need to push; not append
+        // Update front end otherwise teams wont appear removed
+        this.$set(this.debatesById[fromDebate.id], 'teams', fromDebate.teams)
+
+      } else if (fromDebate === 'unused') {
+
+        // Moving to a debate
+        if (toDebate.teams[toPosition]) {
+          // If replacing a team move them to unused
+          this.unallocatedItems.push(toDebate.teams[toPosition])
+        }
+        // Set the move and update the front end
+        toDebate.teams[toPosition] = team
+        this.$set(this.debatesById[toDebate.id], 'teams', toDebate.teams)
+        // Remove from unallocated
+        this.unallocatedItems.splice(this.unallocatedItems.indexOf(team), 1)
+
       }
       // Saving
       var debatesToSave = this.determineDebatesToSave(fromDebate, toDebate)
