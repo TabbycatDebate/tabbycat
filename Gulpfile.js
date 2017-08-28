660f411e7d15fe5b020e5934f09f6f03a3f28050
Philip Belesky <contact@philipbelesky.com>
2017-08-28 11:42:12 +1000

    Remove lato, add interface

--- Gulpfile.js
+++ Gulpfile.js
@@ -37,8 +37,6 @@ gulp.task('fonts-compile', function() {
       'node_modules/bootstrap-sass/assets/fonts/**/*.ttf',
       'node_modules/bootstrap-sass/assets/fonts/**/*.woff',
       'node_modules/bootstrap-sass/assets/fonts/**/*.woff2',
-      'node_modules/lato-font/fonts/lato-normal/lato-normal.woff',
-      'node_modules/lato-font/fonts/lato-normal/lato-normal.woff2'
     ])
     .pipe(rename({dirname: ''})) // Remove folder structure
     .pipe(gulp.dest(outputDir + 'fonts/'));
