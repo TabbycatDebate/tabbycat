c4d06b2a0519d08c2c214427a619e35bd437de96
Philip Belesky <contact@philipbelesky.com>
2017-09-24 20:42:02 +1000

    Fix minor error with results form validator
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- tabbycat/templates/js-standalones/enter_results.js
+++ tabbycat/templates/js-standalones/enter_results.js
@@ -180,7 +180,7 @@ function update_speaker() {
 
 $("#resultsForm").validate({
   invalidHandler: function(event, validator) {
-    $.fn.resetButton('.')
+    $.fn.resetButton(event.target)
   }
 });
 
