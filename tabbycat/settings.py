dbf1d3ecbb1e759f1e651b498d372834019b769d
Chuan-Zheng Lee <czlee@stanford.edu>
2017-09-26 15:52:14 -0700

    Bump version to 1.4.4

--- tabbycat/settings.py
+++ tabbycat/settings.py
@@ -23,9 +23,9 @@ TIME_ZONE = 'Australia/Melbourne'
 LANGUAGE_CODE = 'en'
 USE_I18N = True
 
-TABBYCAT_VERSION = '1.4.3'
+TABBYCAT_VERSION = '1.4.4'
 TABBYCAT_CODENAME = 'Havana Brown'
-READTHEDOCS_VERSION = 'v1.4.3'
+READTHEDOCS_VERSION = 'v1.4.4'
 
 LOCALE_PATHS = [
     os.path.join(BASE_DIR, 'locale'),
