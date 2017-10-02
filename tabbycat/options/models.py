2b6cdb84a7dfc02f11038bb7a9e8520f90310ea7
Chuan-Zheng Lee <czlee@stanford.edu>
2017-10-02 00:12:06 -0400

    i18n remaining models

--- tabbycat/options/models.py
+++ tabbycat/options/models.py
@@ -1,19 +1,23 @@
 from django.db import models
-from tournaments.models import Tournament
+from django.utils.translation import ugettext_lazy as _
 from dynamic_preferences.models import PerInstancePreferenceModel
 from dynamic_preferences.registries import PerInstancePreferenceRegistry, preference_models
+
+from tournaments.models import Tournament
 
 tournament_preferences_registry = PerInstancePreferenceRegistry()
 
 
 class TournamentPreferenceModel(PerInstancePreferenceModel):
 
-    instance = models.ForeignKey(Tournament, models.CASCADE, related_name="preferences")
+    instance = models.ForeignKey(Tournament, models.CASCADE, related_name="preferences",
+        verbose_name=_("instance"))
     registry = tournament_preferences_registry
 
     class Meta(PerInstancePreferenceModel.Meta):
         app_label = "options"
-        verbose_name = "tournament preference"
+        verbose_name = _("tournament preference")
+        verbose_name_plural = _("tournament preferences")
 
 
 preference_models.register(TournamentPreferenceModel,
