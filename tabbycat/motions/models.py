35cbd0c06c5f7c00c8d15237582ee5dc06c0e487
Philip Belesky <contact@philipbelesky.com>
2017-10-05 20:19:24 +0700

    Add info slides into public motion views
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- tabbycat/motions/models.py
+++ tabbycat/motions/models.py
@@ -15,7 +15,7 @@ class Motion(models.Model):
         verbose_name=_("reference"),
         help_text=_("Shortcode for the motion, e.g., \"Bananas\""))
     info_slide = models.TextField(
-        verbose_name=_("info slide"), default="",
+        verbose_name=_("info slide"), default="", blank=True,
         help_text=_("The information slide for this topic; if it has one"))
     flagged = models.BooleanField(default=False,
         verbose_name=_("flagged"),
