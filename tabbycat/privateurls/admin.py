304093e3ccdbf502b2b59586300024b22a93f9bf
Chuan-Zheng Lee <czlee@stanford.edu>
2017-10-28 10:26:26 -0400

    Add search fields to privateurls admin

--- tabbycat/privateurls/admin.py
+++ tabbycat/privateurls/admin.py
@@ -9,6 +9,7 @@ class PrivateUrlSentMailRecordAdmin(admin.ModelAdmin):
     list_display = ('recipient_name', 'get_team', 'url_type', 'email', 'url_key', 'timestamp')
     list_filter = ('url_type',)
     ordering = ('timestamp',)
+    search_fields = ('email', 'speaker__name', 'adjudicator__name')
 
     def get_team(self, obj):
         if obj.speaker:
