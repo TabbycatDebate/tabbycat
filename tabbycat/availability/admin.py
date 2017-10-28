092ad99d428fc06f40f3feb46eef3b95cd592b4c
Philip Belesky <contact@philipbelesky.com>
2017-10-28 21:05:10 +1100

    Remove searching for availabilities as content_object is not searchable/crashes

--- tabbycat/availability/admin.py
+++ tabbycat/availability/admin.py
@@ -5,7 +5,6 @@ from .models import RoundAvailability
 
 class RoundAvailabilityAdmin(admin.ModelAdmin):
     list_display = ('content_object', 'content_type', 'round')
-    search_fields = ('content_object', 'round')
     list_filter = ('content_type', 'round')
 
 admin.site.register(RoundAvailability, RoundAvailabilityAdmin)
