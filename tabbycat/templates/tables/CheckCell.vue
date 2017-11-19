c19fa154d490e5ceea40231c810e2da305d45eed
Philip Belesky <contact@philipbelesky.com>
2017-11-19 15:14:12 +1100

    Resolve ambiguous checkbox state in #521

--- tabbycat/templates/tables/CheckCell.vue
+++ tabbycat/templates/tables/CheckCell.vue
@@ -5,10 +5,9 @@
     <span v-if="cellData['sort']" hidden>
       {{ cellData.checked }}
     </span>
-    <label class="custom-control custom-checkbox">
-      <input type="checkbox" class="custom-control-input" @click="checkUpdate"
-             v-model.lazy="cellData.checked">
-      <span class="custom-control-indicator"></span>
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input" @change="checkUpdate"
+             v-model.boolean.lazy="cellData.checked">
     </label>
 
   </td>
@@ -26,21 +25,19 @@ export default {
   },
   methods: {
     checkUpdate: function () {
-      this.$nextTick(function() { // Wait for model to update
-        var cd = this.cellData
-        var checked = cd.checked // This is currently the pre-clicked value
-        // Updates can be sent off individually via this component itself; or by
-        // communicating back up to the Coordinating Vue Container (where they
-        // can be handled in bulk or issue via a single bulk method)
-        if (_.isUndefined(this.cellData.saveURL)) {
-          this.$eventHub.$emit('toggle-checked', cd.id, checked, cd.type)
-        } else {
-          var message = cd.id + "'s " + cd.type + " status as " + checked
-          var payload = { id: cd.id }
-          payload[cd.type] = checked
-          this.ajaxSave(cd.saveURL, payload, message, null, null, null)
-        }
-      })
+      var cd = this.cellData
+      var checked = cd.checked // This is currently the pre-clicked value
+      // Updates can be sent off individually via this component itself; or by
+      // communicating back up to the Coordinating Vue Container (where they
+      // can be handled in bulk or issue via a single bulk method)
+      if (_.isUndefined(this.cellData.saveURL)) {
+        this.$eventHub.$emit('toggle-checked', cd.id, checked, cd.type)
+      } else {
+        var message = cd.id + "'s " + cd.type + " status as " + checked
+        var payload = { id: cd.id }
+        payload[cd.type] = checked
+        this.ajaxSave(cd.saveURL, payload, message, null, null, null)
+      }
     }
   },
 }
