05c05f0c3183128fb89d10bb39c90d9818b36096
Philip Belesky <contact@philipbelesky.com>
2017-11-27 11:14:13 +1100

    Update version/changelog for hotfix release

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ author = 'Philip Belesky, Chuan-Zheng Lee'
 # The short X.Y version.
 version = '2.0'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.1'
+release = '2.0.2'
 
 rst_epilog = """
 .. |vrelease| replace:: v{release}
