cb50ccc1a7191fb982a6653cbb5954b03b9e86ba
daigo-kimura <a91381@gmail.com>
2018-01-22 01:52:14 +0900

    Add adj score validation test

--- tabbycat/participants/tests/test_models.py
+++ tabbycat/participants/tests/test_models.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+
 from utils.tests import BaseDebateTestCase
 
 from participants.models import Adjudicator, Institution
@@ -11,3 +13,15 @@ class TestInstitution(BaseDebateTestCase):
 class TestAdjudicator(BaseDebateTestCase):
     def test_objects(self):
         self.failUnlessEqual(8, Adjudicator.objects.count())
+
+    def test_invalid_score(self):
+        adj = Adjudicator(
+            tournament=self.t,
+            name="Adjudicator X",
+            test_score=self.t.pref('adj_min_score')-1)
+        self.assertRaises(ValidationError, adj.clean)
+        adj = Adjudicator(
+            tournament=self.t,
+            name="Adjudicator X",
+            test_score=self.t.pref('adj_max_score')+1)
+        self.assertRaises(ValidationError, adj.clean)
