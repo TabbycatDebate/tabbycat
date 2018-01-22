9abd4714306b6865bb33fc7ecff12e8bb6dda7f4
daigo-kimura <a91381@gmail.com>
2018-01-22 14:16:48 +0900

    Revert "Add adj score validation test"
    This reverts commit cb50ccc1a7191fb982a6653cbb5954b03b9e86ba.

--- tabbycat/participants/tests/test_models.py
+++ tabbycat/participants/tests/test_models.py
@@ -1,5 +1,3 @@
-from django.core.exceptions import ValidationError
-
 from utils.tests import BaseDebateTestCase
 
 from participants.models import Adjudicator, Institution
@@ -13,15 +11,3 @@ class TestInstitution(BaseDebateTestCase):
 class TestAdjudicator(BaseDebateTestCase):
     def test_objects(self):
         self.failUnlessEqual(8, Adjudicator.objects.count())
-
-    def test_invalid_score(self):
-        adj = Adjudicator(
-            tournament=self.t,
-            name="Adjudicator X",
-            test_score=self.t.pref('adj_min_score')-1)
-        self.assertRaises(ValidationError, adj.clean)
-        adj = Adjudicator(
-            tournament=self.t,
-            name="Adjudicator X",
-            test_score=self.t.pref('adj_max_score')+1)
-        self.assertRaises(ValidationError, adj.clean)
