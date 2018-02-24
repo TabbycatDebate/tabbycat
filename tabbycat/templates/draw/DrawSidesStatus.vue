17cda08cd186f0278e36a6ae073baa1e8569837a
Philip Belesky <contact@philipbelesky.com>
2018-02-24 12:58:22 +1100

    Fix sides table spacing

--- tabbycat/templates/draw/DrawSidesStatus.vue
+++ tabbycat/templates/draw/DrawSidesStatus.vue
@@ -3,7 +3,7 @@
   <div class="draw-cell">
     <div class="mt-1 ml-2">
 
-      <label class="form-check-label m-0">
+      <label class="form-check-label m-0 pl-3">
         <input type="checkbox" class="form-check-input"
                v-model.boolean.lazy="debate.sidesConfirmed" @change="checkUpdate">
         <span :class="[debate.sidesConfirmed ? 'text-success' : 'text-danger']">
