414259280ca2e852231ed41cdb8f627eedb636cf
Philip Belesky <contact@philipbelesky.com>
2018-03-09 19:11:01 +1100

    Disable popovers only for the admin/assistant draw views (not public)

--- tabbycat/draw/views.py
+++ tabbycat/draw/views.py
@@ -156,7 +156,7 @@ class PublicAllDrawsAllTournamentsView(PublicTournamentPageMixin, CacheMixin, Ba
 # Draw Alerts Utilities (Admin)
 # ==============================================================================
 
-class AdminDrawUtiltiiesMixin:
+class AdminDrawUtiltiesMixin:
     """ Shared between the Admin Draw page and Admin Display Page"""
 
     def get_draw(self):
@@ -194,16 +194,23 @@ class AdminDrawUtiltiiesMixin:
 # Viewing Draw (Admin)
 # ==============================================================================
 
-class BaseDrawDisplayView(AdminDrawUtiltiiesMixin, RoundMixin, TemplateView):
+class BaseDrawDisplayView(AdminDrawUtiltiesMixin, RoundMixin, TemplateView):
     pass
 
 
-class BaseDrawDisplayForRoundByVenueView(BaseDrawTableView):
+class BaseNonPublicDrawDrawTableView(BaseDrawTableView):
+
+    def get_context_data(self, **kwargs):
+        kwargs['no_popovers'] = True  # Note: can't do so in BaseDrawDisplayView
+        return super().get_context_data(**kwargs)
+
+
+class BaseDrawDisplayForRoundByVenueView(BaseNonPublicDrawDrawTableView):
     # inherit everything, this class is kept in code for ease of reading
     pass
 
 
-class BaseDrawDisplayForRoundByTeamView(BaseDrawTableView):
+class BaseDrawDisplayForRoundByTeamView(BaseNonPublicDrawDrawTableView):
 
     sort_key = '' # Leave with default sort order
 
@@ -247,7 +254,7 @@ class AssistantDrawDisplayForRoundByTeamView(CurrentRoundMixin, OptionalAssistan
 # Draw Creation (Admin)
 # ==============================================================================
 
-class AdminDrawView(RoundMixin, AdministratorMixin, AdminDrawUtiltiiesMixin, VueTableTemplateView):
+class AdminDrawView(RoundMixin, AdministratorMixin, AdminDrawUtiltiesMixin, VueTableTemplateView):
     detailed = False
 
     def get_page_title(self):
