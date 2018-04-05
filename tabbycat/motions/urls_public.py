7749d0ddaec465367a3f7efa261eebf44955657a
Philip Belesky <contact@philipbelesky.com>
2018-04-05 18:15:33 +1000

    Fix missing motion standings url; improve 2 team motion standings diagrams

--- tabbycat/motions/urls_public.py
+++ tabbycat/motions/urls_public.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PublicMotionsView.as_view(), name='motions-public'),
-    path('statistics/', views.PublicMotionStatisticsView.as_view(), name='motions-public-statistics'),
+    path('',
+        views.PublicMotionsView.as_view(),
+        name='motions-public'),
+    path('statistics/',
+        views.PublicMotionStatisticsView.as_view(),
+        name='motions-public-statistics'),
 ]
