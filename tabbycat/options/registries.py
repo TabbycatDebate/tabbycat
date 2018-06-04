707777b745e73f54c411bb03db6ac23bcaaf8f17
Chuan-Zheng Lee <czlee@stanford.edu>
2018-06-04 01:11:30 -0700

    Change preference model binding to pattern in documentation
    See:
    https://django-dynamic-preferences.readthedocs.io/en/latest/bind_preferences_to_models.html#create-a-registry-to-collect-your-model-preferences
    (cherry picked from commit bbba071456ce753f466d3616e8aaff2df96c3dd3)

---
+++ tabbycat/options/registries.py
@@ -0,0 +1,8 @@
+from dynamic_preferences.registries import PerInstancePreferenceRegistry
+
+
+class TournamentPreferenceRegistry(PerInstancePreferenceRegistry):
+    pass
+
+
+tournament_preferences_registry = TournamentPreferenceRegistry()
