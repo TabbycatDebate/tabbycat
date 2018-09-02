59e1a0b1c735d80609a2e292a8bef8efb99fcb9c
Philip Belesky <contact@philipbelesky.com>
2018-09-02 21:06:13 +1000

    Fold request history into development settings

--- tabbycat/settings.py
+++ tabbycat/settings.py
@@ -427,6 +427,7 @@ if os.environ.get('TRAVIS', '') == 'true':
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 DEBUG_TOOLBAR_PANELS = (
+    'ddt_request_history.panels.request_history.RequestHistoryPanel',
     'debug_toolbar.panels.versions.VersionsPanel',
     'debug_toolbar.panels.timer.TimerPanel',
     'debug_toolbar.panels.settings.SettingsPanel',
