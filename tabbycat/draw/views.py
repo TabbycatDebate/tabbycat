97a43d586d99971849d0f82f71c585f86c9ac1d6
Étienne Beaulé <beauleetienne0@gmail.com>
2018-10-04 14:48:03 -0400

    Add alert to online ballot entry on iron
    A new alert is created for ballot entry forms for the case where a team
    had iron-manned in the previous round.

--- tabbycat/draw/views.py
+++ tabbycat/draw/views.py
@@ -14,7 +14,6 @@ from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.utils.functional import cached_property
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
-from django.utils.text import format_lazy
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy, ngettext
 from django.views.generic.base import TemplateView
