71fbd2b0bf2a8bed75521ac355edd6b2d4f98144
Étienne Beaulé <beauleetienne0@gmail.com>
2018-11-28 09:29:31 -0500

    Add template tag for adjudicator record links
    Would make reading the HTML a bit easier. Also added the links to items
    in conflict lists.

--- tabbycat/participants/templatetags/participant_link.py
+++ tabbycat/participants/templatetags/participant_link.py
@@ -8,7 +8,7 @@ register = template.Library()
 
 
 @register.simple_tag
-def team_record_link(team, admin, tournament=None):
+def team_record_link(team, admin, tournament=None, style=True):
     """Team record links are used often, so this template tag just reduces
     clutter in templates, in particular in translated strings."""
 
@@ -28,4 +28,26 @@ def team_record_link(team, admin, tournament=None):
     else:
         url = reverse_tournament('participants-public-team-record', tournament, kwargs={'pk': team.pk})
 
-    return mark_safe("""<a href="%(url)s" class="list-group-item-text alert-link">%(name)s</a>""" % {'url': url, 'name': name})
+    classes = 'class="list-group-item-text alert-link"' if style else ''
+
+    return mark_safe("""<a href="%(url)s" %(style)s>%(name)s</a>""" % {'url': url, 'style': classes, 'name': name})
+
+
+@register.simple_tag
+def adj_record_link(adj, admin, tournament=None):
+
+    if not adj:
+        return ""
+
+    if not tournament:
+        tournament = getattr(adj, 'tournament', None)
+
+    if not tournament: # Still no tournament
+        return ""
+
+    if admin:
+        url = reverse_tournament('participants-adjudicator-record', tournament, kwargs={'pk': adj.pk})
+    else:
+        url = reverse_tournament('participants-public-adjudicator-record', tournament, kwargs={'pk': adj.pk})
+
+    return mark_safe(url)
