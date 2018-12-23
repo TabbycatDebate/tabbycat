2f3c4e8dae689d0c7f4edab87d0a76fa374412e6
Philip Belesky <contact@philipbelesky.com>
2018-12-23 16:11:33 +0200

    Add incomplete panel checks, close FRONTEND-8S
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- tabbycat/adjallocation/templates/DraggableAllocation.vue
+++ tabbycat/adjallocation/templates/DraggableAllocation.vue
@@ -7,7 +7,7 @@
     <div class="d-flex vc-chair-flex flex-truncate flex-nowrap border-left">
       <droppable-item class="d-flex flex-grow-1" :handle-drop="handleDebateOrPanelDrop"
                       :drop-context="{ assignment: debateOrPanel.id, position: 'C'}">
-        <div :class="['align-items-center flex-fill', chairID ? 'd-none' : 'd-flex']">
+        <div :class="['align-items-center flex-fill', chairID ? 'd-none' : 'd-flex panel-incomplete']">
           <div class="mx-auto py-2 px-3">ⓒ</div>
         </div>
         <draggable-adjudicator v-if="chairID" class="flex-fill" :item="allAdjudicators[chairID]"
@@ -16,8 +16,9 @@
         </draggable-adjudicator>
       </droppable-item>
     </div>
-    <div class="d-flex flex-grow-1 border-left">
-      <droppable-item class="d-flex flex-grow-1 flex-wrap" :handle-drop="handleDebateOrPanelDrop"
+    <div :class="'d-flex flex-grow-1 border-left'">
+      <droppable-item :class="['d-flex flex-grow-1 flex-wrap', adjudicators.P.length % 2 ? 'panel-incomplete' : '']"
+                      :handle-drop="handleDebateOrPanelDrop"
                       :drop-context="{ assignment: debateOrPanel.id, position: 'P'}">
         <draggable-adjudicator v-for="adjID in adjudicators.P" :item="allAdjudicators[adjID]"
                                :drag-payload="getDragPayload(adjID, 'P')" :key="adjID">
--- tabbycat/draw/templates/InlineTeam.vue
+++ tabbycat/draw/templates/InlineTeam.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="text-truncate small py-1 px-2 flex-fill" :class="highlightsCSS">
+  <div class="text-truncate small py-1 px-2 flex-fill" :class="highlightsCSS"
+       @mouseenter="showHoverPanel(team, 'team')" @mouseleave="hideHoverPanel">
     {{ team.short_name }}
   </div>
 </template>
 
 <script>
 import HighlightableMixin from '../../utils/templates/HighlightableMixin.vue'
+import HoverableMixin from '../../utils/templates/HoverableMixin.vue'
 
 export default {
-  mixins: [HighlightableMixin],
+  mixins: [HighlightableMixin, HoverableMixin],
   props: { team: Object },
   computed: {
     highlightData: function () {
