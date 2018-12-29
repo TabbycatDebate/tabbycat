9592dd9d9ec99cf86b45b25ae5d0a4b71e4e72a3
Chuan-Zheng Lee <czlee@stanford.edu>
2018-12-29 13:39:54 +0200

    Make populate_win_counts default to 0 not None

--- tabbycat/participants/prefetch.py
+++ tabbycat/participants/prefetch.py
@@ -25,9 +25,9 @@ def populate_win_counts(teams, round=None):
         teams_by_id[team.id]._points = team.points_annotation
 
     for team in teams:
-        if not hasattr(team, '_wins_count'):
+        if getattr(team, '_wins_count', None) is None:
             team._wins_count = 0
-        if not hasattr(team, '_points'):
+        if getattr(team, '_points', None) is None:
             team._points = 0
 
 
