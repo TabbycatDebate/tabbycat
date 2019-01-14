51f63caee8d6856d44e37bf78cd9308b46516ce4
viran <viran.w@icloud.com>
2019-01-14 21:21:18 +1100

    Minor cap calculator adjustment

--- tabbycat/breakqual/aida.py
+++ tabbycat/breakqual/aida.py
@@ -150,7 +150,7 @@ class Aida2019AustralsBreakGenerator(BaseAida2016AustralsBreakGenerator):
         super().compute_break()
 
     def calculate_cap(self):
-        additional_teams = self.break_size - 16 if self.break_size - 16 >= 0 else 0
+        additional_teams = self.break_size - 16 if self.break_size >= 16 else 0
         self.institution_cap = 3 + math.floor(additional_teams / 8)
         logger.info("Break size of %d teams exceeds the 16-team base by %d. The institutional cap is set at %d.",self.break_size,additional_teams, self.institution_cap)
     
