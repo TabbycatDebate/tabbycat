dc61a7ab36b56027b6cffae0fb1edc3562706194
Chuan-Zheng Lee <czlee@stanford.edu>
2019-06-09 20:08:33 -0700

    Add very basic standings tests

--- tabbycat/standings/tests/test_standings.py
+++ tabbycat/standings/tests/test_standings.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from ..base import StandingsError
 from ..teams import TeamStandingsGenerator
 
 from adjallocation.models import DebateAdjudicator
@@ -15,6 +16,74 @@ from venues.models import Venue
 
 # TODO Have classes to check each metric annotator in isolation
 # TODO Have classes to check each ranking annotator in isolation
+
+
+class TestTrivialStandings(TestCase):
+    """Tests cases with just two teams and one round.
+
+    Mostly intended to check that it doesn't crash under lots of different
+    configurations, rather than check the results of the ordering or aggregation
+    functions themselves."""
+
+    def setUp(self):
+        self.tournament = Tournament.objects.create(slug="trivialstandingstest", name="Trivial standings test")
+        self.team1 = Team.objects.create(tournament=self.tournament, reference="1", use_institution_prefix=False)
+        self.team2 = Team.objects.create(tournament=self.tournament, reference="2", use_institution_prefix=False)
+        adj = Adjudicator.objects.create(tournament=self.tournament, name="Adjudicator")
+        rd = Round.objects.create(tournament=self.tournament, seq=1)
+        debate = Debate.objects.create(round=rd)
+        self.dt0 = DebateTeam.objects.create(debate=debate, team=self.team1, side=DebateTeam.SIDE_AFF)
+        self.dt1 = DebateTeam.objects.create(debate=debate, team=self.team2, side=DebateTeam.SIDE_NEG)
+        DebateAdjudicator.objects.create(debate=debate, adjudicator=adj, type=DebateAdjudicator.TYPE_CHAIR)
+        ballotsub = BallotSubmission.objects.create(debate=debate, confirmed=True)
+        TeamScore.objects.create(debate_team=self.dt0, ballot_submission=ballotsub,
+            margin=+1, points=1, score=263, win=True,  votes_given=1, votes_possible=1)
+        TeamScore.objects.create(debate_team=self.dt1, ballot_submission=ballotsub,
+            margin=-1, points=0, score=261, win=False, votes_given=0, votes_possible=1)
+
+    def tearDown(self):
+        self.dt0.delete()
+        self.dt1.delete()
+        self.tournament.delete()
+
+    def test_no_rankings(self):
+        generator = TeamStandingsGenerator(('points',), ())
+        standings = generator.generate(self.tournament.team_set.all())
+        self.assertEqual(standings.get_standing(self.team1).metrics['points'], 1)
+        self.assertEqual(standings.get_standing(self.team2).metrics['points'], 0)
+
+    def test_points(self):
+        generator = TeamStandingsGenerator(('points',), ('rank',))
+        standings = generator.generate(self.tournament.team_set.all())
+        self.assertEqual(standings.get_standing(self.team1).metrics['points'], 1)
+        self.assertEqual(standings.get_standing(self.team2).metrics['points'], 0)
+        self.assertEqual(standings.get_standing(self.team1).rankings['rank'], (1, False))
+        self.assertEqual(standings.get_standing(self.team2).rankings['rank'], (2, False))
+
+    def test_speaks(self):
+        generator = TeamStandingsGenerator(('speaks_sum',), ('rank',))
+        standings = generator.generate(self.tournament.team_set.all())
+        self.assertEqual(standings.get_standing(self.team1).metrics['speaks_sum'], 263)
+        self.assertEqual(standings.get_standing(self.team2).metrics['speaks_sum'], 261)
+        self.assertEqual(standings.get_standing(self.team1).rankings['rank'], (1, False))
+        self.assertEqual(standings.get_standing(self.team2).rankings['rank'], (2, False))
+
+    def test_points_speaks_subrank(self):
+        generator = TeamStandingsGenerator(('points', 'speaks_sum'), ('rank', 'subrank'))
+        standings = generator.generate(self.tournament.team_set.all())
+        self.assertEqual(standings.get_standing(self.team1).metrics['points'], 1)
+        self.assertEqual(standings.get_standing(self.team2).metrics['points'], 0)
+        self.assertEqual(standings.get_standing(self.team1).metrics['speaks_sum'], 263)
+        self.assertEqual(standings.get_standing(self.team2).metrics['speaks_sum'], 261)
+        self.assertEqual(standings.get_standing(self.team1).rankings['subrank'], (1, False))
+        self.assertEqual(standings.get_standing(self.team2).rankings['subrank'], (1, False))
+
+    def test_wbw_first_error(self):
+        self.assertRaises(ValueError, TeamStandingsGenerator, ('wbw', 'points'), ('rank',))
+
+    def test_double_metric_error(self):
+        self.assertRaises(StandingsError, TeamStandingsGenerator, ('points', 'wbw', 'points'), ('rank',))
+
 
 class TestBasicStandings(TestCase):
 
@@ -91,9 +160,6 @@ class TestBasicStandings(TestCase):
 
     def teardown_testdata(self, tournament):
         tournament.delete()
-
-    def test_invalid_wbw(self):
-        self.assertRaises(ValueError, TeamStandingsGenerator, ('wbw', 'points'), ('rank'))
 
     def test_standings(self):
         for index, testdata in self.testdata.items():
