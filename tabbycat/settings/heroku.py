766334a10764fcd70433a02959cf491a6f4c4421
Chuan-Zheng Lee <czlee@stanford.edu>
2019-06-13 10:57:13 -0700

    Override sentry_sdk.serializer.MAX_DATABAG_BREADTH
    This should allow the dict sent in extra data in Sentry reports to
    include all preferences, rather than being trimmed to 10 items.
    
    https://forum.sentry.io/t/python-sdk-extra-data-capped-at-400-characters/6909

--- tabbycat/settings/heroku.py
+++ tabbycat/settings/heroku.py
@@ -121,6 +121,10 @@ else:
         send_default_pii=True
     )
 
+    # Override dictionary trimming so that all preferences will be included in Sentry reports
+    # https://forum.sentry.io/t/python-sdk-extra-data-capped-at-400-characters/6909
+    sentry_sdk.serializer.MAX_DATABAG_BREADTH = 200
+
 # ==============================================================================
 # Scout
 # ==============================================================================
