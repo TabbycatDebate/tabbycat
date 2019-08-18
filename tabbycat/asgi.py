706942cb0671765fe15c7a8ca0b8d5d614074e58
Philip Belesky <contact@philipbelesky.com>
2019-08-18 16:45:18 +1000

    Instrument channels infrastructure with Sentry

--- tabbycat/asgi.py
+++ tabbycat/asgi.py
@@ -6,12 +6,16 @@ defined in the ASGI_APPLICATION setting.
 import os
 
 import django
+import sentry_sdk
 
 from channels.routing import get_default_application
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+sentry_sdk.init(dsn="https://6bf2099f349542f4b9baf73ca9789597@sentry.io/185382")
 
 django.setup()
 
-application = get_default_application()
+# Wrap ASGI middleware; as per docs.sentry.io/platforms/python/asgi/
+application = SentryAsgiMiddleware(get_default_application())
