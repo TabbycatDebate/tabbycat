11e3630fb24a271b32dafeb268de45f9d24b1410
Étienne Beaulé <beauleetienne0@gmail.com>
2019-09-06 14:10:35 -0400

    Move random identifier generation to utils.misc
    As used in various places: private URLs and checkin identifiers.

--- tabbycat/privateurls/utils.py
+++ tabbycat/privateurls/utils.py
@@ -1,24 +1,20 @@
 import logging
 import string
-from secrets import SystemRandom
 
 from participants.models import Person
+from utils.misc import generate_identifier_string
 
 logger = logging.getLogger(__name__)
 
 
-def generate_url_key(length=8):
-    """Generates a randomised URL key."""
-    chars = string.ascii_lowercase + string.digits
-    return ''.join(SystemRandom().choice(chars) for _ in range(length))
-
-
 def populate_url_keys(people, length=8, num_attempts=10):
     """Populates the URL key field for every instance in the given QuerySet."""
+    chars = string.ascii_lowercase + string.digits
+
     existing_keys = list(Person.objects.exclude(url_key__isnull=True).values_list('url_key', flat=True))
     for person in people:
         for i in range(num_attempts):
-            new_key = generate_url_key(length)
+            new_key = generate_identifier_string(chars, length)
             if new_key not in existing_keys:
                 person.url_key = new_key
                 existing_keys.append(new_key)
