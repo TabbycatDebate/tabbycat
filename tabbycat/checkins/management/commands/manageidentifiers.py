d87fde98adaced613045b8ee80c07c3e9b689bcc
Étienne Beaulé <beauleetienne0@gmail.com>
2019-09-07 20:14:14 -0400

    Fixup manageidentifiers command
    - The 'length' argument is obsolete and removed.
    - The options are no longer made into an attribute
    - create_identifiers() gets the model to create from.
    - The model parameter is available for the delete subparser

--- tabbycat/checkins/management/commands/manageidentifiers.py
+++ tabbycat/checkins/management/commands/manageidentifiers.py
@@ -19,40 +19,37 @@ class Command(TournamentCommand):
         subparsers.required = True
 
         generate = subparsers.add_parser("generate")
-        super(Command, self).add_arguments(generate)
-        generate.add_argument('-l', '--length', type=int, default=6,
-            help="Length of URL key to generate (default 6)")
+        super().add_arguments(generate)
         generate.add_argument('-O', '--overwrite', action="store_true", default=False,
             help="Overwrite existing URL keys")
         generate.add_argument('-m', '--model', type=str, choices=['person', 'venue'], default='person',
-            help="Which model for whoch to create identifiers")
+            help="Which model to use to create identifiers")
 
         delete = subparsers.add_parser("delete")
-        super(Command, self).add_arguments(delete)
+        super().add_arguments(delete)
+        delete.add_argument('-m', '--model', type=str, choices=['person', 'venue'], default='person',
+            help="Which model to use to create identifiers")
 
     def handle_tournament(self, tournament, **options):
-        self.options = options
-
         model = {
             'person': Person,
             'venue': Venue,
-        }[self.options['model']]
+        }[options['model']]
+        related_model = model.checkin_identifier.related.related_model
 
         model_query = model.objects.all()
-        if self.options['model'] == 'venue':
+        if options['model'] == 'venue':
             model_query = model_query.filter(tournament=tournament)
         else:
             model_query = model_query.filter(Q(adjudicator__tournament=tournament) | Q(speaker__team__tournament=tournament))
 
-        if options['subcommand'] == "generate" and self.options['overwrite'] or options['subcommand'] == "delete":
-            self.stdout.write("Deleting all check-in identifiers for {0:s}...".format(self.options['model']))
+        if options['subcommand'] == "generate" and options['overwrite'] or options['subcommand'] == "delete":
+            self.stdout.write("Deleting all check-in identifiers for {0:s}...".format(options['model']))
             delete_identifiers(model_query)
 
         if options['subcommand'] == "generate":
-            self.stdout.write("Creating all check-in identifiers for {0:s}...".format(self.options['model']))
-            if self.options['overwrite']:
-                delete_identifiers(model_query.filter(checkin_identifier__isnull=False))
+            self.stdout.write("Creating all check-in identifiers for {0:s}...".format(options['model']))
 
             queryset = model_query.filter(checkin_identifier__isnull=True)
-            self.stdout.write("Generating check-in identifiers for {0:d} {1:s}".format(queryset.count(), self.options['model']))
-            create_identifiers(queryset, length=self.options['length'])
+            self.stdout.write("Generating check-in identifiers for {0:d} {1:s}".format(queryset.count(), options['model']))
+            create_identifiers(related_model, queryset)
