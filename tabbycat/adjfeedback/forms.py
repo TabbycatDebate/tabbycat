e210b39ba7ad1d895a0ceff505eb83c5c946eed6
Philip Belesky <contact@philipbelesky.com>
2019-10-27 16:46:57 +1100

    Produce actual behaviour desired in 3b3bfe

--- tabbycat/adjfeedback/forms.py
+++ tabbycat/adjfeedback/forms.py
@@ -156,9 +156,13 @@ class BaseFeedbackForm(forms.Form):
         elif question.answer_type == question.ANSWER_TYPE_MULTIPLE_SELECT:
             field = AdjudicatorFeedbackCheckboxSelectMultipleField(choices=question.choices_for_field)
         field.label = question.text
-        if question.required:
-            field.label += "*"
-        field.required = self._enforce_required and question.required
+
+        # Required checkbox fields don't really make sense; so override the behaviour?
+        if question.answer_type != question.ANSWER_TYPE_BOOLEAN_CHECKBOX:
+            if question.required:
+                field.label += "*"
+            field.required = self._enforce_required and question.required
+
         return field
 
     def _create_fields(self):
