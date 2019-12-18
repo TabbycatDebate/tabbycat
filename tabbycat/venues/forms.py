628f5969e220c80fd307d97d0fab7dbc0ac13b87
Étienne Beaulé <beauleetienne0@gmail.com>
2019-12-18 01:46:57 -0500

    Sort venues in category formset by display name
    This sorting would place venues with the same (already existing)
    category prefix together, making the form easier to use.

--- tabbycat/venues/forms.py
+++ tabbycat/venues/forms.py
@@ -5,7 +5,10 @@ from .models import VenueCategory
 
 def venuecategoryform_factory(venues_queryset):
 
-    venue_choices = [(venue.id, venue.display_name) for venue in venues_queryset]
+    venue_choices = sorted(
+        [(venue.id, venue.display_name) for venue in venues_queryset],
+        key=lambda k: k[1]  # Sort by display name
+    )
 
     class VenueCategoryForm(ModelForm):
 
