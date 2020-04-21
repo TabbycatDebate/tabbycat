6e16f3e8b83ec3432758bce85853a1fe5dc72510
Étienne Beaulé <beauleetienne0@gmail.com>
2020-04-21 00:00:29 -0300

    Remove trailing slashes from API endpoints
    As supported by the REST naming conventions. This supersedes the Django
    naming as there shouldn't be much interaction between users of both and
    so no confusion.

--- tabbycat/api/urls.py
+++ tabbycat/api/urls.py
@@ -16,122 +16,122 @@ urlpatterns = [
         views.APIRootView.as_view(),
         name='api-root'),
 
-    path('tournaments/', include([
+    path('tournaments', include([
 
         path('',
             views.TournamentViewSet.as_view(list_methods),
             name='api-tournament-list'),
 
-        path('<slug:tournament_slug>/', include([
+        path('/<slug:tournament_slug>', include([
 
             path('',
                 views.TournamentViewSet.as_view(detail_methods),
                 name='api-tournament-detail'),
 
-            path('rounds/', include([
+            path('/rounds', include([
                 path('',
                     views.RoundViewSet.as_view(list_methods),
                     name='api-round-list'),
 
-                path('<int:round_seq>/', include([
+                path('/<int:round_seq>', include([
                     path('',
                         views.RoundViewSet.as_view(detail_methods),
                         name='api-round-detail'),
 
-                    path('pairings/', include([
+                    path('/pairings', include([
                         path('',
                             views.PairingViewSet.as_view(list_methods),
                             name='api-pairing-list'),
-                        path('<int:pk>/',
+                        path('/<int:pk>',
                             views.PairingViewSet.as_view(detail_methods),
                             name='api-pairing-detail'),
                     ])),
                 ])),
             ])),
 
-            path('break-categories/', include([
+            path('/break-categories', include([
 
                 path('',
                     views.BreakCategoryViewSet.as_view(list_methods),
                     name='api-breakcategory-list'),
 
-                path('<int:pk>/', include([
+                path('/<int:pk>', include([
                     path('',
                         views.BreakCategoryViewSet.as_view(detail_methods),
                         name='api-breakcategory-detail'),
-                    path('eligibility/',
+                    path('/eligibility',
                         views.BreakEligibilityView.as_view(),
                         name='api-breakcategory-eligibility'),
                 ])),
             ])),
 
-            path('speaker-categories/', include([
+            path('/speaker-categories', include([
 
                 path('',
                     views.SpeakerCategoryViewSet.as_view(list_methods),
                     name='api-speakercategory-list'),
 
-                path('<int:pk>/', include([
+                path('/<int:pk>', include([
                     path('',
                         views.SpeakerCategoryViewSet.as_view(detail_methods),
                         name='api-speakercategory-detail'),
-                    path('eligibility/',
+                    path('/eligibility',
                         views.SpeakerEligibilityView.as_view(),
                         name='api-speakercategory-eligibility'),
                 ])),
             ])),
 
-            path('institutions/', include([
+            path('/institutions', include([
                 path('',
                      views.InstitutionViewSet.as_view(list_methods),
                      name='api-institution-list'),
-                path('<int:pk>/',
+                path('/<int:pk>',
                      views.InstitutionViewSet.as_view(detail_methods),
                      name='api-institution-detail'),
             ])),
-            path('teams/', include([
+            path('/teams', include([
                 path('',
                      views.TeamViewSet.as_view(list_methods),
                      name='api-team-list'),
-                path('<int:pk>/',
+                path('/<int:pk>',
                      views.TeamViewSet.as_view(detail_methods),
                      name='api-team-detail'),
-                path('standings/',
+                path('/standings',
                      views.TeamStandingsView.as_view(),
                      name='api-team-standings'),
             ])),
-            path('adjudicators/', include([
+            path('/adjudicators', include([
                 path('',
                      views.AdjudicatorViewSet.as_view(list_methods),
                      name='api-adjudicator-list'),
-                path('<int:pk>/',
+                path('/<int:pk>',
                      views.AdjudicatorViewSet.as_view(detail_methods),
                      name='api-adjudicator-detail'),
             ])),
-            path('speakers/', include([
+            path('/speakers', include([
                 path('',
                      views.SpeakerViewSet.as_view(list_methods),
                      name='api-speaker-list'),
-                path('<int:pk>/',
+                path('/<int:pk>',
                      views.SpeakerViewSet.as_view(detail_methods),
                      name='api-speaker-detail'),
-                path('standings/',
+                path('/standings',
                      views.SpeakerStandingsView.as_view(),
                      name='api-speaker-standings'),
             ])),
-            path('venues/', include([
+            path('/venues', include([
                 path('',
                     views.VenueViewSet.as_view(list_methods),
                     name='api-venue-list'),
-                path('<int:pk>/',
+                path('/<int:pk>',
                     views.VenueViewSet.as_view(detail_methods),
                     name='api-venue-detail'),
             ])),
-            path('venue-categories/', include([
+            path('/venue-categories', include([
                 path('',
                     views.VenueCategoryViewSet.as_view(list_methods),
                     name='api-venuecategory-list'),
-                path('<int:pk>/',
+                path('/<int:pk>',
                     views.VenueCategoryViewSet.as_view(detail_methods),
                     name='api-venuecategory-detail'),
             ])),
@@ -141,11 +141,11 @@ urlpatterns = [
 
 
     ])),
-    path('institutions/', include([
+    path('/institutions', include([
         path('',
              views.GlobalInstitutionViewSet.as_view(list_methods),
              name='api-global-institution-list'),
-        path('<int:pk>/',
+        path('/<int:pk>',
              views.GlobalInstitutionViewSet.as_view(detail_methods),
              name='api-global-institution-detail'),
     ])),
