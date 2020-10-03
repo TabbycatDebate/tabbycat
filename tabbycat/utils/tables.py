ec578d32ddecde3aaf20f8a93e571684c4fd5acb
Chuan-Zheng Lee <czlee@stanford.edu>
2020-10-03 22:07:57 +1300

    Fix debate flags display error and form field
    This fixes a couple of issues that probably arose as a result of #1525.
    
    1. Currently, the draw page displays the characters of `str(debate.flags)`
    on one line each (so "[", "]" for no flags). This commit fixes it so
    that it just displays the human-readable versions of the flags, restoring
    the behavior before #1525 was merged.
    
    2. Currently, the form field for flags in the Edit Database area rejects
    the value whenever one or more fields are selected. This commit modifies
    the flags fields to use a new `ChoiceArrayField` that is like an
    `ArrayField`, but handles choices using a `TypedMultipleChoiceField`.
    
    The ChoiceArrayField code was taken from:
    https://gist.github.com/danni/f55c4ce19598b2b345ef
    
    See also related Django issue tracker:
    https://code.djangoproject.com/ticket/27704
    
    I couldn't figure out how to (easily) get the form field in the Edit
    Database area to be taller than 2.28571rem, the height of most fields
    in django-jet, without an exception made for select[multiple].

--- tabbycat/utils/tables.py
+++ tabbycat/utils/tables.py
@@ -8,6 +8,7 @@ from django.utils.translation import gettext as _
 from django.utils.translation import ngettext
 
 from adjallocation.allocation import AdjudicatorAllocation
+from draw.generator import DRAW_FLAG_DESCRIPTIONS
 from options.utils import use_team_code_names
 from standings.templatetags.standingsformat import metricformat, rankingformat
 from tournaments.mixins import SingleObjectByRandomisedUrlMixin
@@ -17,6 +18,7 @@ from utils.misc import reverse_round, reverse_tournament
 from .mixins import AdministratorMixin
 
 logger = logging.getLogger(__name__)
+_draw_flags_dict = dict(DRAW_FLAG_DESCRIPTIONS)
 
 
 class BaseTableBuilder:
@@ -745,9 +747,11 @@ class TabbycatTableBuilder(BaseTableBuilder):
         conflicts_by_debate = []
         for debate in debates:
             # conflicts is a list of (level, message) tuples
-            conflicts = [("secondary", flag) for flag in debate.get_flags_display()]
-            conflicts += [("secondary", "%(team)s: %(flag)s" % {'team': self._team_short_name(debate.get_team(side)), 'flag': flag})
-                    for side in self.tournament.sides for flag in debate.get_dt(side).get_flags_display()]
+            conflicts = [("secondary", _draw_flags_dict.get(flag, flag)) for flag in debate.flags]
+            conflicts += [("secondary", "%(team)s: %(flag)s" % {
+                        'team': self._team_short_name(debate.get_team(side)),
+                        'flag': _draw_flags_dict.get(flag, flag),
+                    }) for side in self.tournament.sides for flag in debate.get_dt(side).flags]
 
             if self.tournament.pref('avoid_team_history'):
                 history = debate.history
