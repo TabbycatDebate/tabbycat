8de87c2a46799be8055a8a7f1111cd895626808b
Étienne Beaulé <beauleetienne0@gmail.com>
2021-01-13 15:01:24 -0500

    Migrate participant ballots for concurrent rounds
    With #516, there can be many rounds happening at the same time, but it
    is not reflected in the public ballot submission views, of which
    silently fail, showing a wrong ballot, if adjudicators are assigned to
    concurrent rounds.
    
    To fix, the views are now Round-based with the sequence in the URL, with
    links split per-round.

--- tabbycat/results/urls_public.py
+++ tabbycat/results/urls_public.py
@@ -7,27 +7,29 @@ urlpatterns = [
     path('',
         views.PublicResultsIndexView.as_view(),
         name='results-public-index'),
-    path('round/<int:round_seq>/',
-        views.PublicResultsForRoundView.as_view(),
-        name='results-public-round'),
     path('debate/<int:pk>/scoresheets/',
         views.PublicBallotScoresheetsView.as_view(),
         name='results-public-scoresheet-view'),
+    path('round/<int:round_seq>/', include([
+        path('',
+            views.PublicResultsForRoundView.as_view(),
+            name='results-public-round'),
 
-    # Public Ballots
-    path('add/',
-        views.PublicBallotSubmissionIndexView.as_view(),
-        name='results-public-ballot-submission-index'),
-    path('add/adjudicator/<int:adj_id>/',
-        views.OldPublicNewBallotSetByIdUrlView.as_view(),
-        name='old-results-public-ballotset-new-pk'),
+        # Public Ballots
+        path('add/',
+            views.PublicBallotSubmissionIndexView.as_view(),
+            name='results-public-ballot-submission-index'),
+        path('add/adjudicator/<int:adjudicator_pk>/',
+            views.OldPublicNewBallotSetByIdUrlView.as_view(),
+            name='old-results-public-ballotset-new-pk'),
+    ])),
 
     # Private Ballots
-    path('adjudicator/<slug:url_key>/', include([
+    path('adjudicator/<slug:url_key>/round/<int:round_seq>/', include([
         path('add/',
             views.OldPublicNewBallotSetByRandomisedUrlView.as_view(),
             name='results-public-ballotset-new-randomised'),
-        path('round/<int:round_seq>/view/',
+        path('view/',
             views.PrivateUrlBallotScoresheetView.as_view(),
             name='results-privateurl-scoresheet-view'),
     ])),
