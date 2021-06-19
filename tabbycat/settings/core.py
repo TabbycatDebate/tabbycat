00172d8f5b87e54417ed387f2905e2abb3c817dc
Étienne Beaulé <beauleetienne0@gmail.com>
2021-06-19 00:02:05 -0400

    Set LOGOUT_REDIRECT_URL to avoid path override

--- tabbycat/settings/core.py
+++ tabbycat/settings/core.py
@@ -156,6 +156,7 @@ INSTALLED_APPS = (
 
 ROOT_URLCONF = 'urls'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 FIXTURE_DIRS = (os.path.join(os.path.dirname(BASE_DIR), 'data', 'fixtures'), )
 SILENCED_SYSTEM_CHECKS = ('urls.W002',)
 
--- tabbycat/users/urls.py
+++ tabbycat/users/urls.py
@@ -1,14 +1,9 @@
-from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from . import views
 
 
 urlpatterns = [
-    path('logout/',
-        auth_views.LogoutView.as_view(),
-        {'next_page': '/'},  # override to specify next_page
-        name='logout'),
     path('',
         include('django.contrib.auth.urls')),
     path('signup/<slug:key>/', views.SignUpView.as_view(), name='signup'),
