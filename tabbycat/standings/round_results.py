fbc7bbdf429acbef5021c194fb74dd002117738c
Étienne Beaulé <beauleetienne0@gmail.com>
2021-11-18 00:27:30 -0400

    Add weighting to round points results
    This commit is in two parts:
    * Use the PointsAnnotator in the points_count Team property for weights;
    * Add weights for public round results.
    
    For the latter, the check for a non-null points value is replaced with
    an "or" coalesce, and the round weight is taken through relations, like
    in the function populating the list.

--- tabbycat/standings/round_results.py
+++ tabbycat/standings/round_results.py
@@ -60,7 +60,7 @@ def add_team_round_results_public(teams, rounds, opponents=False):
     add_team_round_results(teams, rounds, opponents=opponents,
         lookup=(lambda teams, x: [t for t in teams if t == x][0]), id_attr='id')
     for team in teams:
-        team.points = sum([ts.points for ts in team.round_results if ts and ts.points is not None])
+        team.points = sum([(ts.points or 0) * ts.debate_team.debate.round.weight for ts in team.round_results if ts is not None])
 
 
 def add_speaker_round_results(standings, rounds, tournament, replies=False):
