800aea8fb30d29691004a2d5243a10af756ace1a
Philip Belesky <philip@belesky.com>
2022-01-15 17:58:46 +1100

    Minor redesign of allocation modal (first draft)

--- tabbycat/templates/modals/ModalForAllocating.vue
+++ tabbycat/templates/modals/ModalForAllocating.vue
@@ -3,91 +3,107 @@
   <div class="modal fade" :id="id" tabindex="-1" role="dialog" aria-hidden="true" ref="modal">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
-        <div class="modal-body text-center p-4">
+        <div class="modal-body text-center p-4 bg-bg">
 
-          <p class="lead" v-text="introText"></p>
+          <p class="font-italic" v-text="introText"></p>
 
-          <div v-if="!forPanels && extra.hasPreformedPanels && !notUsingPreformed">
-            <p v-text="gettext(`You can automatically allocate adjudicators to debates by either
-                                assigning preformed panels or by using the standard
-                                auto-allocator method that places individual adjudicators.`)"></p>
-            <button type="submit" @click="smartAllocateWithPreformed"
-                    :class="['btn btn-block btn-success', loading ? 'disabled': '']"
-                    v-text="loading ? gettext('Loading...') : gettext('Smart Allocate Preformed Panels')"></button>
-            <button type="submit" @click="directAllocateWithPreformed"
-                    :class="['btn btn-block btn-success', loading ? 'disabled': '']"
-                    v-text="loading ? gettext('Loading...') : gettext('Direct Allocate Preformed Panels')"></button>
-            <button type="submit" @click="notUsingPreformed = true" class="btn btn-block btn-success mt-4"
-                    v-text="gettext('Allocate Individual Adjudicators')"></button>
+          <div class="card">
+            <div class="card-body ">
+              <h5 class="card-title mb-0" v-text="gettext(`Auto-Allocate Preformed Panels`)"></h5>
+            </div>
+            <div class="list-group list-group-item-flush" v-if="!forPanels && extra.hasPreformedPanels">
+              <div class="list-group-item p-3">
+                <button type="submit" @click="smartAllocateWithPreformed"
+                      :class="['btn btn-block btn-success', loading ? 'disabled': '']"
+                      v-text="loading ? gettext('Loading...') : gettext('Smart Allocate')"></button>
+                <p class="font-italic mt-1 mb-1" v-text="gettext(`Allocates the highest-priority preformed panels to the highest-priority matchups.`)"></p>
+              </div>
+              <div class="list-group-item p-3">
+                <button type="submit" @click="directAllocateWithPreformed"
+                      :class="['btn btn-block btn-success', loading ? 'disabled': '']"
+                      v-text="loading ? gettext('Loading...') : gettext('Direct Allocate')"></button>
+                <p class="font-italic mt-1 mb-1" v-text="gettext(`Allocates the highest-priority preformed panels to the draw going from top to bottom.`)"></p>
+              </div>
+            </div>
+            <div v-if="!forPanels && !extra.hasPreformedPanels">
+              <p class="font-italic mb-3" v-text="gettext(`No preformed panels exist for this round. You can create some by going to Setup, and then Preformed Panels.`)"></p>
+            </div>
           </div>
 
-          <div v-if="notUsingPreformed || forPanels || !extra.hasPreformedPanels">
+          <div class="card mt-3">
 
-            <p v-text="gettext(`The allocator creates stronger panels for debates that were given
-                                higher importances. If importances have not been set it will allocate
-                                stronger panels to debates in a higher bracket.`)"></p>
-
-            <div v-if="settings.draw_rules__adj_min_voting_score > extra.adjMaxScore" class="alert alert-warning">
-              The score required to be allocated as voting panellist ({{ settings.draw_rules__adj_min_voting_score }}) is
-              higher than the maximum adjudicator score ({{ extra.adjMaxScore }}).
-            </div>
-            <div v-if="settings.draw_rules__adj_min_voting_score < extra.adjMinScore" class="alert alert-warning">
-              The score required to be allocated as voting panellist ({{ settings.draw_rules__adj_min_voting_score }}) is
-              lower than the minimum  adjudicator score ({{ extra.adjMinScore }}).
-            </div>
-            <div class="text-left py-3">
-              <div class="form-group row">
-                <div class="col-sm-3">
-                  <input v-model.number="settings.draw_rules__adj_min_voting_score" type="number" class="form-control">
-                </div>
-                <label class="col-sm-9 col-form-label"
-                       v-text="gettext('Minimum feedback score required to be allocated as chair or panellist')"></label>
-              </div>
-              <div class="form-group row">
-                <div class="col-sm-3">
-                  <input v-model=settings.draw_rules__no_panellist_position type="checkbox" class="form-control">
-                </div>
-                <label class="col-sm-9 col-form-label" v-text="gettext('Do not allocate panellists')"></label>
-              </div>
-              <div class="form-group row">
-                <div class="col-sm-3">
-                  <input v-model=settings.draw_rules__no_trainee_position type="checkbox" class="form-control">
-                </div>
-                <label class="col-sm-9 col-form-label" v-text="gettext('Do not allocate trainees')"></label>
-              </div>
-              <div class="form-group row">
-                <div class="col-sm-3">
-                  <input v-model.number=settings.draw_rules__adj_history_penalty type="number" class="form-control">
-                </div>
-                <label class="col-sm-9 col-form-label"
-                       v-text="gettext(`History penalty — higher numbers will more strongly avoid
-                                        matching adjudicators to teams or panellists they have seen
-                                        before`)">
-                </label>
-              </div>
-              <div class="form-group row">
-                <div class="col-sm-3">
-                  <input v-model.number=settings.draw_rules__adj_conflict_penalty type="number" class="form-control">
-                </div>
-                <label class="col-sm-9 col-form-label" v-text="gettext('Conflict penalty — higher numbers will more strongly avoid recorded conflicts')"></label>
-              </div>
-              <div class="form-group row" v-if="forPanels">
-                <div class="col-sm-3">
-                  <input v-model.number=settings.draw_rules__preformed_panel_mismatch_penalty type="number" class="form-control">
-                </div>
-                <label class="col-sm-9 col-form-label"
-                       v-text="gettext(`Importance mismatch penalty — higher numbers will more
-                                        strongly match panel strengths to assigned importances`)">
-                </label>
-              </div>
+            <div class="card-body">
+              <h5 class="card-title mb-0" v-text="gettext(`Auto-Allocate Individual Adjudicators`)"></h5>
             </div>
 
-            <button type="submit" @click="allocateWithInfill"
-                    :class="['btn btn-block btn-success', loading ? 'disabled': '']"
-                    v-text="loading ? gettext('Loading...') : gettext('Auto-Allocate Adjudicators')"></button>
+            <div v-if="!forPanels" class="list-group list-group-item py-0">
 
+              <div v-if="settings.draw_rules__adj_min_voting_score > extra.adjMaxScore" class="alert alert-warning">
+                The score required to be allocated as voting panellist ({{ settings.draw_rules__adj_min_voting_score }}) is
+                higher than the maximum adjudicator score ({{ extra.adjMaxScore }}).
+              </div>
+              <div v-if="settings.draw_rules__adj_min_voting_score < extra.adjMinScore" class="alert alert-warning">
+                The score required to be allocated as voting panellist ({{ settings.draw_rules__adj_min_voting_score }}) is
+                lower than the minimum  adjudicator score ({{ extra.adjMinScore }}).
+              </div>
+              <div class="text-left py-3">
+                <div class="form-group row">
+                  <div class="col-sm-3">
+                    <input v-model.number="settings.draw_rules__adj_min_voting_score" type="number" class="form-control">
+                  </div>
+                  <label class="col-sm-9 col-form-label"
+                         v-text="gettext('Minimum feedback score required to be a chair or panellist')"></label>
+                </div>
+                <div class="form-group row">
+                  <div class="col-sm-3">
+                    <input v-model=settings.draw_rules__no_panellist_position type="checkbox" class="form-control">
+                  </div>
+                  <label class="col-sm-9 col-form-label" v-text="gettext('Do not allocate panellists')"></label>
+                </div>
+                <div class="form-group row">
+                  <div class="col-sm-3">
+                    <input v-model=settings.draw_rules__no_trainee_position type="checkbox" class="form-control">
+                  </div>
+                  <label class="col-sm-9 col-form-label" v-text="gettext('Do not allocate trainees')"></label>
+                </div>
+                <div class="form-group row">
+                  <div class="col-sm-3">
+                    <input v-model.number=settings.draw_rules__adj_history_penalty type="number" class="form-control">
+                  </div>
+                  <label class="col-sm-9 col-form-label"
+                         v-text="gettext(`History penalty — higher numbers will more strongly avoid
+                                          matching adjudicators to teams or panellists they have seen
+                                          before`)">
+                  </label>
+                </div>
+                <div class="form-group row">
+                  <div class="col-sm-3">
+                    <input v-model.number=settings.draw_rules__adj_conflict_penalty type="number" class="form-control">
+                  </div>
+                  <label class="col-sm-9 col-form-label" v-text="gettext('Conflict penalty — higher numbers will more strongly avoid recorded conflicts')"></label>
+                </div>
+                <div class="form-group row" v-if="forPanels">
+                  <div class="col-sm-3">
+                    <input v-model.number=settings.draw_rules__preformed_panel_mismatch_penalty type="number" class="form-control">
+                  </div>
+                  <label class="col-sm-9 col-form-label"
+                         v-text="gettext(`Importance mismatch penalty — higher numbers will more
+                                          strongly match panel strengths to assigned importances`)">
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <div class="list-group-item pt-2 px-3 pb-0">
+
+              <button type="submit" @click="allocateIndividualAdjs"
+                      :class="['btn btn-block btn-success my-2', loading ? 'disabled': '']"
+                      v-text="loading ? gettext('Loading...') : gettext('Auto-Allocate Adjudicators')"></button>
+              <p class="font-italic" v-text="gettext(`The allocator creates stronger panels for debates that were given
+                                  higher importances. If importances have not been set it will allocate
+                                  stronger panels to debates in a higher bracket.`)"></p>
+            </div>
           </div>
-
         </div>
       </div>
     </div>
@@ -117,7 +133,6 @@ export default {
     return {
       id: 'confirmAllocateModal',
       settings: null,
-      notUsingPreformed: false,
     }
   },
   created: function () {
@@ -135,7 +150,7 @@ export default {
       this.settings.allocationMethod = 'direct'
       this.performWSAction(this.settings)
     },
-    allocateWithInfill: function () {
+    allocateIndividualAdjs: function () {
       this.settings.usePreformedPanels = false
       this.performWSAction(this.settings)
     },
