429d9b61c020c24ef54f10113a80e340b7a7dbdc
Étienne Beaulé <beauleetienne0@gmail.com>
2022-09-02 00:14:34 -0300

    Add built-in password requirements
    Fixes #1700

--- tabbycat/settings/core.py
+++ tabbycat/settings/core.py
@@ -347,3 +347,22 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_URLS_REGEX = r'^/api(/.*)?$'
+
+# ==============================================================================
+# Password validators
+# ==============================================================================
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
