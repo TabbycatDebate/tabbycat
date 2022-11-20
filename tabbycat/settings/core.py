cb38cdbe62aa5305fd4110a62484504295039736
Étienne Beaulé <beauleetienne0@gmail.com>
2022-11-20 15:20:06 -0400

    Remove pagination from the API
    The pagination changes the schema when applied and messes up the docu-
    mentation. Don't think it was ever used either.

--- tabbycat/settings/core.py
+++ tabbycat/settings/core.py
@@ -333,7 +333,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
