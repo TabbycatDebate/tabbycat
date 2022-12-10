970120087d12f647dbe1c4d412a16155028524e9
Étienne Beaulé <etienne@dashhudson.com>
2022-12-10 12:45:07 -0400

    Add Tabbycat logo to API docs

--- tabbycat/settings/core.py
+++ tabbycat/settings/core.py
@@ -345,6 +345,12 @@ SPECTACULAR_SETTINGS = {
     'SCHEMA_PATH_PREFIX': r'api/v\d+',
     'CONTACT': {'name': 'Étienne Beaulé', 'email': 'ebeaule@tabbycat-debate.org'},
     'LICENSE': {'name': 'AGPL 3', 'url': 'https://www.gnu.org/licenses/agpl-3.0.en.html'},
+    'EXTENSIONS_INFO': {
+        "x-logo": {
+            "url": "/static/logo.svg",
+            "altText": "Tabbycat logo",
+        },
+    }
 }
 
 # ----------------------------------------
