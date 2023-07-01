0796ac34999a4601b7bb57e146c3109013afb266
Étienne Beaulé <beauleetienne0@gmail.com>
2023-07-01 16:42:56 -0300

    Create tests for option views

--- tabbycat/options/tests/test_views.py
+++ tabbycat/options/tests/test_views.py
@@ -1,10 +1,12 @@
 from unittest.mock import patch
 
+from django.contrib.auth import get_user_model
 from django.http.response import Http404
 from django.test import RequestFactory, TestCase
 
-from options.presets import PreferencesPreset
-from options.views import SetPresetPreferencesView
+from options.preferences import scoring
+from options.presets import PreferencesPreset, PublicInformation
+from options.views import SetPresetPreferencesView, TournamentConfigIndexView, TournamentPreferenceFormView
 from tournaments.models import Tournament
 from utils.misc import reverse_tournament
 
@@ -17,6 +19,61 @@ class TestPreset(PreferencesPreset):
     # Scoring
     scoring__score_min = 70
     scoring__score_max = 80
+
+
+class TournamentConfigIndexViewTests(TestCase):
+
+    def test_order_presets(self):
+        view = TournamentConfigIndexView()
+        view.setup(RequestFactory())
+
+        presets = view.get_context_data()['presets']
+        self.assertEqual(presets[0], PublicInformation)
+
+
+class TournamentPreferenceFormViewTests(TestCase):
+
+    @patch('options.views.tournament_preference_form_builder')
+    def test_gets_correct_form(self, mock_builder):
+        tournament = Tournament.objects.create(slug="optionform", name="Option Form Testing")
+        view = TournamentPreferenceFormView()
+        view.setup(RequestFactory().get(
+            reverse_tournament('options-tournament-section', tournament, kwargs={'section': 'scoring'})), tournament_slug=tournament.slug, section='scoring',
+        )
+        view.get_form_class()
+        mock_builder.assert_called_with(instance=tournament, section='scoring')
+        tournament.delete()
+
+    def test_section_does_not_exist(self):
+        tournament = Tournament.objects.create(slug="optionform", name="Option Form Testing")
+        tournament.round_set.create(seq=1, name='1', abbreviation='1', draw_type='M')
+        request = RequestFactory().get(reverse_tournament('options-tournament-section', tournament, kwargs={'section': 'invalidsection'}))
+        request.user = get_user_model()(is_superuser=True)
+
+        view = TournamentPreferenceFormView()
+        view.setup(request, tournament_slug=tournament.slug, section='invalidsection')
+
+        with self.assertRaises(Http404):
+            view.dispatch(request, section='invalidsection')
+
+        tournament.delete()
+
+    @patch('options.views.messages.success')
+    @patch('django.views.generic.edit.HttpResponseRedirect')
+    def test_save_form(self, mock_redirect, mock_success):
+        tournament = Tournament.objects.create(slug="optionform", name="Option Form Testing")
+        request = RequestFactory().get(reverse_tournament('options-tournament-section', tournament, kwargs={'section': 'scoring'}))
+        view = TournamentPreferenceFormView()
+        view.setup(request, tournament_slug=tournament.slug, section='scoring')
+        view.section = scoring
+
+        form = TestPreset.get_form(tournament, data={'scoring__score_min': 70, 'scoring__score_max': 80})
+        form.is_valid()
+        view.form_valid(form)
+        mock_success.assert_called()
+        mock_redirect.assert_called_with(reverse_tournament('options-tournament-index', tournament))
+
+        tournament.delete()
 
 
 class TestSetPresetPreferencesView(TestCase):
