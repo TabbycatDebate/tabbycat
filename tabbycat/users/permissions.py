2ff095e483af62de132385be35151b2ed5b0a582
Valerie Pang <valeriepang22@gmail.com>
2023-08-28 16:45:31 -0300

    Add extra permissions

--- tabbycat/users/permissions.py
+++ tabbycat/users/permissions.py
@@ -60,6 +60,9 @@ class Permission(TextChoices):
     EDIT_DEBATETEAMS = 'edit.debateteam', _("edit debate teams (pairings)")
     VIEW_DEBATEADJUDICATORS = 'view.debateadjudicator', _("view debate adjudicators (allocations)")
     EDIT_DEBATEADJUDICATORS = 'edit.debateadjudicator', _("edit debate adjudicators (allocations)")
+    VIEW_ROOMALLOCATIONS = 'view.roomallocations', _("view room allocations")
+    EDIT_ROOMALLOCATIONS = 'edit.roomallocations', _("edit room allocations")
+    EDIT_ALLOCATESIDES = 'edit.allocatesides', _("edit and confirm outround team positions")
 
     # Logic behind the ballotsub permissions:
     # Confirmed ballots are more prominent than old ones, but are more sensitive to changes.
@@ -73,5 +76,55 @@ class Permission(TextChoices):
     MARK_OTHERS_BALLOTSUBMISSIONS = 'mark.ballotsubmission.others', _("confirm/discard others' ballots")
     VIEW_BALLOTSUBMISSION_GRAPH = 'view.ballotsubmission.graph', _("view ballot graph")
 
+    VIEW_MOTION = 'view.roundmotion', _("view motion per round")
+    EDIT_MOTION = 'edit.roundmotion', _("edit motion per round")
+    EDIT_RELEASEDRAW = 'edit.releasedraw', _("release draw to public")
+    EDIT_RELEASEMOTION = 'edit.releasemotion', _("release motion to public")
+    EDIT_STARTTIME = 'edit.starttime', _("add debate start time")
+    # these permissions are already assigned to the assistants
+    VIEW_ALLCURRENTDRAWS_ROOM = 'view.allcurrentdrawsroom', _("view all current draws by room")
+    VIEW_ALLCURRENTDRAWS_TEAM = 'view.allcurrentdrawsteam', _("view all current draws by team")
+    VIEW_ROUNDDRAW_ROOM = 'view.rounddrawroom', _("view current round draw by room")
+    VIEW_ROUNDDRAW_TEAM = 'view.rounddrawteam', _("view current round draw by team")
+    VIEW_DISPLAYMOTION = 'view.displaymotion', _("view display room motion page")
+
     VIEW_TOURNAMENTPREFERENCEMODEL = 'view.tournamentpreferencemodel', _("view tournament configuration")
     EDIT_TOURNAMENTPREFERENCEMODEL = 'edit.tournamentpreferencemodel', _("edit tournament configuration")
+
+    VIEW_PREFORMEDPANELS = 'view.preformedpanels', _("view existing preformed panels")
+    EDIT_PREFORMEDPANELS = 'edit.preformedpanels', _("edit existing preformed panels")
+
+    # standings tab
+    VIEW_STANDINGS_OVERVIEW = 'view.standingsoverview', _("view the overviews of standings")
+    VIEW_TEAMSTANDINGS = 'view.teamstandings', _("view the most recent team standings")
+    VIEW_SPEAKERSSTANDINGS = 'view.speakersstandings', _("view the most recent speaker standings")
+    VIEW_REPLIESSTANDINGS = 'view.repliesstandings', _("view the most recent replies standings")
+    VIEW_MOTIONSTAB = 'view.motionstab', _("view the most recent motions tab")
+    VIEW_DIVERSITYTAB = 'view.diversitytab', _("view the diversity tab")
+
+    # Feedback tab
+    VIEW_FEEDBACK_OVERVIEW = 'view.feedbackoverview', _("view overview of judge feedback scores")
+    EDIT_FEEDBACK_OVERVIEW = 'edit.feedbackoverview', _("edit overview of judge feedback scores")
+    # not sure what the right most column of the overview page is called, but I'm calling it comments for now
+    EDIT_JUDGESCORES_BULK = 'edit.judgescoresbulk', _("bulk update judge scores")
+    EDIT_BASEJUDGESCORES_IND = 'edit.judgescoresind', _("edit base scores of judges")
+    EDIT_SETBREAKING = 'edit.setbreaking', _("edit breaking judges")
+    VIEW_FEEDBACK_LATEST = 'view.feedbacklatest', _("view the latest feedback tab")
+    VIEW_FEEDBACK_IMPORTANT = 'view.feedbackimportant', _("view the important feedback tab")
+    VIEW_FEEDBACK_COMMENTS = 'view.feedbackcomments', _("view the comments feedback tab")
+    VIEW_FEEDBACK_BYSOURCE = 'view.feedbackbysource', _("view feedback by source")
+    VIEW_FEEDBACK_BYTARGET = 'view.feedbackbytarget', _("view feedback by target")
+    EDIT_FEEDBACK_IGNORE = 'edit.feedbackignore', _("edit the ignore feedback feature")
+    EDIT_FEEDBACK_UNCONFIRM = 'edit.feedbackunconfirm', _("edit the unconfirm feedback feature")
+    VIEW_FEEDBACK_UNSUBMITTED = 'view.feedbackunsubmitted', _("view feedback unsubmitted tab")
+    VIEW_FEEDBACK_ADD = 'view.feedbackadd', _("view add feedback tab")
+    EDIT_FEEDBACK_ADD = 'edit.feedbackadd', _("edit add feedback tab")
+    # idk if its possible for them to add feedback everywhere, considering there is add feedback on multiple pages
+
+    # breaks
+    EDIT_BREAK_ELIGIBILITY = 'edit.breakeligibility', _("edit break eligibility")
+    VIEW_BREAK_ELIGIBILITY = 'view.breakeligibility', _("view break eligibility")
+    EDIT_BREAK_CATEGORIES = 'edit.breakcategories', _("edit break categories")
+    VIEW_BREAK_CATEGORIES = 'view.breakcategories', _("view break categories")
+    GENERATE_BREAK = 'generate.break', _("generate all breaks")
+    EDIT_BREAK_REMARKS = 'edit.breakremarks', _("edit break remarks")
