a7ef6ab62a9a4d744c5dfddf9746a2b40c3b5828
Étienne Beaulé <beauleetienne0@gmail.com>
2024-04-27 11:35:15 -0300

    Remove uses of is_integer() for Decimal instances
    The equivalent of the float .is_integer() method is to compare the
    denominator of the Decimal object with "1". This will avoid the crashes
    due to the former not existing.
    
    It also removes the set_float_or_int() function as it didn't seem to be
    used anywhere.
    
    Fixes BACKEND-C5D

--- tabbycat/results/utils.py
+++ tabbycat/results/utils.py
@@ -77,15 +77,6 @@ def readable_ballotsub_result(debateresult):
     return result_winner, result
 
 
-def set_float_or_int(number, step_value):
-    """Used to ensure the values sent through to the frontend <input> are
-    either Ints or Floats such that the validation can handle them properly"""
-    if step_value.is_integer():
-        return int(number)
-    else:
-        return number
-
-
 def get_result_status_stats(round):
     """Returns a dict where keys are result statuses of debates; values are the
     number of debates in the round with that status.
