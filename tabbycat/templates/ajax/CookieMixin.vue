50c46c4d3a697ef0af80576f6e336c4b92e86398
Teymour Aldridge <teymour@reasoning.page>
2024-12-19 21:20:32 +0100

    Add missing csrf token to "set breaking" button.

---
+++ tabbycat/templates/ajax/CookieMixin.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  methods: {
+    getCookie: function (name) {
+      let cookieValue = null
+      if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';')
+        for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim()
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+          }
+        }
+      }
+      return cookieValue
+    },
+  },
+}
+</script>
