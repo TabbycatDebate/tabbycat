362861141814fe3528739aa209bb3db0e298def9
Timothy Beyer <timothy.beyer@outlook.com>
2025-04-16 12:30:09 +0200

    fixed lint error in forms.py

--- tabbycat/users/forms.py
+++ tabbycat/users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm, UserCreationForm, UsernameField
 from django.utils.translation import gettext_lazy as _
+
 from .models import Membership
 
 
