48a2903a44a53016ae267148939289b17c0d0ce2
eldonmacdonald <157840603+eldonmacdonald@users.noreply.github.com>
2026-07-06 04:19:31 -0300

    Change useSortableTable.js to better handle empty data (#2884)
    * changed useSortableTable.js to better handle empty data
    
    * added tests to make sure that category sort works correctly in standings
    
    * adjusted after_round_4.json to work for new tests
    
    * adjusted to make all tests succeed
    
    * Updated sorting logic to make a tad more sense

--- tabbycat/standings/tests/test_ui.py
+++ tabbycat/standings/tests/test_ui.py
@@ -33,6 +33,41 @@ class CoreStandingsTests(SeleniumTournamentTestCase):
         # TODO: test people of equal ranks
         assert_row_state(rows[50], str(51), "Phil Lyons")
 
+    def test_speaker_categories(self):
+
+        sc1 = self.tournament.speakercategory_set.create(name='sc1', slug='sc1', seq=1, public=True)
+        sc1.speaker_set.set(self.tournament.team_set.first().speaker_set.all())
+
+        sc2 = self.tournament.speakercategory_set.create(name='sc2', slug='sc2', seq=2, public=True)
+        sc2.speaker_set.set(self.tournament.team_set.last().speaker_set.all())
+
+        test_url = self.reverse_url('standings-public-tab-speaker')
+        self.selenium.get('%s%s' % (self.live_server_url, test_url))
+        WebDriverWait(self.selenium, 5).until(
+            lambda driver: driver.find_element_by_css_selector('.table'))
+
+        table = self.selenium.find_elements_by_css_selector(".table")[0]
+
+        tbody = table.find_elements_by_tag_name("tbody")[0]
+        rows = tbody.find_elements_by_tag_name("tr")
+
+        category_sorter = self.selenium.find_element_by_css_selector("[data-original-title='Categories']")
+
+        # Click the category sorter twice to order from highest to lowest priority
+        category_sorter.click()
+        category_sorter.click()
+
+        def assert_row_state(row, rank, name):
+            rank_cell_text = row.find_elements_by_tag_name("td")[0].text
+            name_cell_text = row.find_elements_by_tag_name("td")[1].text
+            self.assertTrue(rank in rank_cell_text)
+            self.assertTrue(name in name_cell_text)
+
+        assert_row_state(rows[0], "10=", "Wanda Griffin")
+        assert_row_state(rows[2], "63=", "Georgia Crawford")
+        assert_row_state(rows[3], "20", "Janie Daniels")
+        assert_row_state(rows[5], "60=", "Javier Chavez")
+
     def test_reply_standings(self):
         test_url = self.reverse_url('standings-public-tab-replies')
         self.selenium.get('%s%s' % (self.live_server_url, test_url))
