cc79193be98da89e70f2bf04ba83ec6302116037
Étienne Beaulé <beauleetienne0@gmail.com>
2026-08-31 10:08:04 -0300

    Port side editing cell to Vue 3

--- tabbycat/templates/tables/SideCell.vue
+++ tabbycat/templates/tables/SideCell.vue
@@ -1,8 +1,37 @@
+<script setup>
+import { computed } from 'vue'
+import { useAjax } from '../composables/useAjax.js'
+
+const props = defineProps({
+  cellData: Object,
+})
+
+const { ajaxSave } = useAjax()
+
+// Coerce null/undefined (unallocated) to '', matching the plain value=""
+// on the Unallocated option.
+const displayValue = computed(() => props.cellData.value ?? '')
+
+const sideUpdate = (newValue) => {
+  if (props.cellData.noSave) {
+    return
+  }
+  const cd = props.cellData
+  const sideValue = newValue === '' ? null : Number(newValue)
+  cd.value = sideValue
+  cd.sort = sideValue
+  const message = `Team ${cd.teamId}'s side for round ${cd.roundId} set to ${sideValue}`
+  const payload = { team_id: cd.teamId, round_id: cd.roundId, side: sideValue }
+  ajaxSave(cd.saveURL, payload, message, null, null, null)
+}
+</script>
+
 <template>
-
-  <td :class="cellData.class ? cellData.class : null" >
-
-    <span v-if="cellData.sort" hidden>
+  <td :class="cellData.class ? cellData.class : null">
+    <span
+      v-if="cellData.sort"
+      hidden
+    >
       {{ cellData.sort }}
     </span>
     <select
@@ -10,47 +39,16 @@
       class="form-control"
       @change="sideUpdate($event.target.value)"
     >
-      <option value="">{{ cellData.unallocatedLabel }}</option>
-      <option v-for="option in cellData.options" :key="option.value" :value="option.value">
+      <option value="">
+        {{ cellData.unallocatedLabel }}
+      </option>
+      <option
+        v-for="option in cellData.options"
+        :key="option.value"
+        :value="option.value"
+      >
         {{ option.label }}
       </option>
     </select>
-
   </td>
-
 </template>
-
-<script>
-import AjaxMixin from '../ajax/AjaxMixin.vue'
-
-export default {
-  mixins: [AjaxMixin],
-  props: {
-    cellData: Object,
-  },
-  computed: {
-    displayValue: function () {
-      // Coerce null/undefined (unallocated) to '', matching the plain
-      // value="" on the Unallocated <option> (a bound :value="null" would
-      // otherwise be stringified to the literal text "null" by the DOM).
-      const value = this.cellData.value
-      return (value === null || value === undefined) ? '' : value
-    },
-  },
-  methods: {
-    sideUpdate: function (newValue) {
-      if (this.cellData.noSave) {
-        return
-      }
-      const cd = this.cellData
-      const sideValue = newValue === '' ? null : Number(newValue)
-      cd.value = sideValue
-      cd.sort = sideValue
-      const message = `Team ${cd.teamId}'s side for round ${cd.roundId} set to ${sideValue}`
-      const payload = { team_id: cd.teamId, round_id: cd.roundId, side: sideValue }
-      this.ajaxSave(cd.saveURL, payload, message, null, null, null)
-    },
-  },
-}
-
-</script>
