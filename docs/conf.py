7afdd247330e83b0b525a8ebb07e47b9b68e6789
Étienne Beaulé <beauleetienne0@gmail.com>
2026-09-23 00:17:27 -0300

    Release 2.12.0

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ author = 'Philip Belesky, Chuan-Zheng Lee, Étienne Beaulé'
 # The short X.Y version.
 version = '2.12'
 # The full version, including alpha/beta/rc tags.
-release = '2.12.0-dev'
+release = '2.12.0'
 
 rst_epilog = """
 .. |vrelease| replace:: v{release}
